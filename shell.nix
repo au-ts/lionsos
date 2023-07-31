1cd74b8534055a0d284e2942159df7104402f9d2
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2023-07-31 13:59:27 +1000

    Fixing up Nix shell and adding dependencies for micropython

--- shell.nix
+++ shell.nix
@@ -1,13 +1,18 @@
 let
     pkgs = import <nixpkgs> {};
+    cross = import <nixpkgs> {
+        crossSystem = { config = "aarch64-none-elf"; };
+    };
 in
   pkgs.mkShell {
     buildInputs = with pkgs.buildPackages; [
+        python39
         unzip
         gnumake
-		dtc
+        dtc
         llvmPackages_16.clang
         llvmPackages_16.lld
+        cross.buildPackages.gcc10
     ];
 }
 
