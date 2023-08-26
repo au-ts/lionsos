ab57fc2a33650d50420d295b0673e61fa22f75bf
Ivan Velickovic <i.velickovic@unsw.edu.au>
2023-08-26 11:24:30 +1000

    Fixing hack in modfb API

--- examples/kitty/src/kitty.py
+++ examples/kitty/src/kitty.py
@@ -8,6 +8,9 @@ while True:
     print("KITTY GRAPHICS about to write")
     for x in range(100, 300):
         for y in range(100, 300):
-            fb.set_pixel(x, y)
-            # fb.set_pixel(x, y, 200-(y-100)/5, 15+(x-100)/2, 100, 0)
+            r = int(200-(y-100)/5)
+            b = int(15+(x-100)/2)
+            g = 100
+            a = 0
+            fb.set_pixel(x, y, (r << 24) & (b << 16) & (g << 8) & a)
     fb.flush()
