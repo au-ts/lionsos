445786268e7085eb4adaf881144347c92995300a
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2023-08-29 12:17:20 +1000

    Fixes in order to have setting framebuffer from MicroPython working

--- examples/kitty/src/kitty.py
+++ examples/kitty/src/kitty.py
@@ -7,9 +7,10 @@ while True:
     print("KITTY GRAPHICS about to write")
     for x in range(100, 300):
         for y in range(100, 300):
-            r = int(200-(y-100)/5)
-            b = int(15+(x-100)/2)
-            g = 100
-            a = 0
-            fb.set_pixel(x, y, (r << 24) & (b << 16) & (g << 8) & a)
+            r = (int(200-(y-100)/5) << 24) & 0xff000000
+            b = (int(15+(x-100)/2) << 16)  & 0x00ff0000
+            g = (int(100) << 8) & 0x0000ff00
+            fb.set_pixel(x, y, r | b | g | a)
+    print("flushing!")
     fb.flush()
+    print("flushed")
