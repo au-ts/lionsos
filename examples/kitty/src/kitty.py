14ce4cf5e5e6a9b9f9ebddab65c9f96666db2ac8
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2023-08-30 10:09:22 +1000

    Fix missing variable declaration in kitty.py

--- examples/kitty/src/kitty.py
+++ examples/kitty/src/kitty.py
@@ -10,6 +10,7 @@ while True:
             r = (int(200-(y-100)/5) << 24) & 0xff000000
             b = (int(15+(x-100)/2) << 16)  & 0x00ff0000
             g = (int(100) << 8) & 0x0000ff00
+            a = 0
             fb.set_pixel(x, y, r | b | g | a)
     print("flushing!")
     fb.flush()
