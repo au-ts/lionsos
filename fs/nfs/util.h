5873f6ccaf92dd1c4ef8b4c39391e35b0d60e50f
James Archer <j.archer@unsw.edu.au>
2023-11-29 13:22:24 +1100

    Remove unused code from NFS client's util.h

--- fs/nfs/util.h
+++ fs/nfs/util.h
@@ -10,25 +10,10 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define STAT 0x98
-#define TRANSMIT 0x40
-#define STAT_TDRE (1 << 14)
-
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
 
-#ifdef __GNUC__
-#define likely(x) __builtin_expect(!!(x), 1)
-#define unlikely(x) __builtin_expect(!!(x), 0)
-#else
-#define likely(x) (!!(x))
-#define unlikely(x) (!!(x))
-#endif
-
-#define MASK(x) ((1 << (x)) - 1)
-#define BIT(x) (1 << (x))
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
-#define ABS(a) ((a) < 0) ? -(a) : (a)
 
 #define dlogp(pred, fmt, ...) do { \
     if (pred) { \
