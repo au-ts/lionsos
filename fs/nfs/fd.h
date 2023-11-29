2b7b647fb7da551a172d1a43261c223bd4980ca7
James Archer <j.archer@unsw.edu.au>
2023-11-29 12:56:56 +1100

    Add copyright notices to NFS client files

--- fs/nfs/fd.h
+++ fs/nfs/fd.h
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2023, UNSW
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
 #pragma once
 
 #include <stdint.h>
--- fs/nfs/nfs.h
+++ fs/nfs/nfs.h
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2023, UNSW
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
 extern struct nfs_context *nfs;
 
 void continuation_pool_init(void);
