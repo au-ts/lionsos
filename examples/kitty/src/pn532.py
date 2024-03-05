a5319e24ecdf0f0052babe4b642cb27db3dc74f9
Alwin Joshy <joshyalwin@gmail.com>
2024-03-06 10:47:02 +1100

    Moved PN532 functionality to class

--- examples/kitty/src/pn532.py
+++ examples/kitty/src/pn532.py
@@ -1,153 +1,174 @@
 from machine import I2C
 import time
 
-PN532_I2C_BUS_ADDRESS = (0x48 >> 1)
+_PN532_I2C_BUS_ADDRESS = (0x48 >> 1)
 
-PN532_CMD_GETFIRMWAREVERSION = 0x02
-PN532_FIRMWAREVERSION_LENGTH = 6
+_DEFAULT_READ_ACK_FRAME_RETRIES = 20
+_DEFAULT_READ_RESPONSE_RETRIES = 100
 
-DEFAULT_READ_ACK_FRAME_RETRIES = 20
-DEFAULT_READ_RESPONSE_RETRIES = 100
+_PN532_CMD_GETFIRMWAREVERSION = 0x02
+_PN532_FIRMWAREVERSION_LENGTH = 6
 
-PN532_PREAMBLE = 0x00
-PN532_STARTCODE1 = 0x00
-PN532_STARTCODE2 = 0xFF
-PN532_POSTAMBLE = 0x00
+_PN532_PREAMBLE = 0x00
+_PN532_STARTCODE1 = 0x00
+_PN532_STARTCODE2 = 0xFF
+_PN532_POSTAMBLE = 0x00
 
-PN532_HOSTTOPN532 = 0xD4
+_PN532_HOSTTOPN532 = 0xD4
 
-PN532_ACK_FRAME_SIZE = 0x07
-PN532_ACK_FRAME = [0, 0, 0xFF, 0, 0xFF, 0]
-PN532_NACK = [0, 0, 0xFF, 0xFF, 0, 0]
+_PN532_ACK_FRAME_SIZE = 0x07
+_PN532_ACK_FRAME = bytearray([0, 0, 0xFF, 0, 0xFF, 0])
+_PN532_NACK = bytearray([0, 0, 0xFF, 0xFF, 0, 0])
 
-def pn532_write_command(header, body, retries):
-	data = bytearray(8 + len(header) + len(body))
+_PN532_CMD_RFCONFIGURATION = 0x32
+_RFCONFIGURATION_HEADER = bytearray([_PN532_CMD_RFCONFIGURATION, 0x5, 0xFF, 0x1, 0xFF])
 
-	data[0] = PN532_PREAMBLE
-	data[1] = PN532_STARTCODE1
-	data[2] = PN532_STARTCODE2
+_PN532_CMD_SAMCONFIGURATION = 0x14
+_SAM_CONFIGURE_HEADER = bytearray([_PN532_CMD_SAMCONFIGURATION, 0x1, 0x14, 0x1])
 
-	length = len(header) + len(body) + 1
-	data[3] = length
-	data[4] = ~length + 1
+_PN532_CMD_INLISTPASSIVETARGET = 0x4a
+_PN532_MIFARE_ISO14443A_BAUD_RATE = 0x0
+_INLISTPASSIVETARGET_HEADER = bytearray([_PN532_CMD_INLISTPASSIVETARGET, 0x1, _PN532_MIFARE_ISO14443A_BAUD_RATE])
 
-	sum = PN532_HOSTTOPN532
-	data[5] = PN532_HOSTTOPN532
+class PN532:
+	def __init__(self, i2c_bus_number):
+		self.i2c_bus = I2C(i2c_bus_number)
 
-	i = 6
-	for elem in header:
-		sum += elem
-		data[i] = elem
+	def _pn532_write_command(self, header, body, retries):
+		data = bytearray(8 + len(header) + len(body))
+
+		data[0] = _PN532_PREAMBLE
+		data[1] = _PN532_STARTCODE1
+		data[2] = _PN532_STARTCODE2
+
+		length = len(header) + len(body) + 1
+		data[3] = length
+		data[4] = ~length + 1
+
+		sum = _PN532_HOSTTOPN532
+		data[5] = _PN532_HOSTTOPN532
+
+		i = 6
+		for elem in header:
+			sum += elem
+			data[i] = elem
+			i += 1
+
+		for elem in body:
+			sum += elem
+			data[i] = elem
+			i += 1
+
+		data[i] = ~sum + 1
 		i += 1
 
-	for elem in body:
-		sum += elem
-		data[i] = elem
-		i += 1
+		data[i] = __PN532_POSTAMBLE
 
-	data[i] = ~sum + 1
-	i += 1
+		self.i2c_bus.writeto(_PN532_I2C_BUS_ADDRESS, data)
+		return self._pn532_read_ack_frame(retries)
 
-	data[i] = PN532_POSTAMBLE
+	def _pn532_read_ack_frame(self, retries):
+		attempts = 0
+		while (attempts < retries):
+			data = self.i2c_bus.readfrom(_PN532_I2C_BUS_ADDRESS, _PN532_ACK_FRAME_SIZE)
 
-	print("about to write to")
-	i2c_bus.writeto(PN532_I2C_BUS_ADDRESS, data)
-	print("finished writing to")
-	return pn532_read_ack_frame(retries)
+			if data[0] & 1:
+				for i in range(0, _PN532_ACK_FRAME_SIZE - 1):
+					value = data[i + 1]
+					if value != _PN532_ACK_FRAME[i]:
+						print("ACK malformed")
+						return -1
 
+				return 0
 
-def pn532_read_ack_frame(retries):
-	attempts = 0
-	while (attempts < retries):
-		data = i2c_bus.readfrom(PN532_I2C_BUS_ADDRESS, PN532_ACK_FRAME_SIZE)
+			attempts += 1
+			time.sleep_ms(1)
 
-		if data[0] & 1:
-			for i in range(0, PN532_ACK_FRAME_SIZE - 1):
-				value = data[i + 1]
-				if value != PN532_ACK_FRAME[i]:
-					print("ACK malformed")
+		print("read_ack_frame: device is not ready yet")
+		return -1
+
+	def _pn532_read_response_length(self, retries):
+		time.sleep_ms(1)
+		length = 0
+		attempts = 0
+
+		while True:
+			data = self.i2c_bus.readfrom(_PN532_I2C_BUS_ADDRESS, 6)
+
+			if (data[0] & 1):
+				length = data[4]
+				break
+			elif attempts == retries:
 					return -1
 
-			return 0
+			attempts += 1
+			time.sleep_ms(1)
 
-		attempts += 1
-		time.sleep_ms(1)
+		# Send NACK
+		i2c_bus.writeto(_PN532_I2C_BUS_ADDRESS, _PN532_NACK)
+		return length
 
-	print("read_ack_frame: device is not ready yet")
-	return -1
-
-def pn532_read_response_length(retries):
-	time.sleep_ms(1)
-	length = 0
-	attempts = 0
-
-	while True:
-		data = i2c_bus.readfrom(PN532_I2C_BUS_ADDRESS, 6)
-
-		if (data[0] & 1):
-			length = data[4]
-			break
-		elif attempts == retries:
-				return -1
-
-		attempts += 1
-		time.sleep_ms(1)
-
-	# Send NACK
-	nack_buf = bytearray(PN532_NACK)
-	i2c_bus.writeto(PN532_I2C_BUS_ADDRESS, nack_buf)
-	return length
-
-def pn532_read_response(retries):
-	length = pn532_read_response_length(retries)
-	if (length < 0):
-		print("READ RESPONSE - Length was less than zero")
-		return []
-
-	attempts = 0
-	num_data_tokens = 7 + length + 2
-	while True:
-		data = i2c_bus.readfrom(PN532_I2C_BUS_ADDRESS, num_data_tokens)
-		if (data[0] & 1):
-			break
-		elif attempts == retries:
-			print("READ RESPONSE - Ran out of attempts")
+	def pn532_read_response(self, retries):
+		length = pn532_read_response_length(retries)
+		if (length < 0):
+			print("READ RESPONSE - Length was less than zero")
 			return []
 
-		attempts += 1
-		time.sleep_ms(1)
+		attempts = 0
+		num_data_tokens = 7 + length + 2
+		while True:
+			data = self.i2c_bus.readfrom(_PN532_I2C_BUS_ADDRESS, num_data_tokens)
+			if (data[0] & 1):
+				break
+			elif attempts == retries:
+				print("READ RESPONSE - Ran out of attempts")
+				return []
 
-	ret_buf = bytearray(length)
-	assert(data[1] == PN532_PREAMBLE, "preamble failed")
-	assert(data[2] == PN532_STARTCODE1, "startcode1 failed")
-	assert(data[3] == PN532_STARTCODE2, "startcode2 failed")
-	assert(data[4] == length, "length failed")
-	assert(data[5] == ~length + 1, "checksum failed") # @alwin: not too sure about this
-	# data[6] is probably PN532_TO_HOST command
-	# data[7] is probably an echo of the command
-	i = 0
-	while i < length:
-		ret_buf[i] = data[8 + i]
-		i += 1
-	i += 8
-	# data[i] is checksum of data
-	# data[i + 1] is postamble
+			attempts += 1
+			time.sleep_ms(1)
 
-	# @alwin: For some reaosn, we need to read another buffer here
+		ret_buf = bytearray(length)
+		assert(data[1] == _PN532_PREAMBLE, "preamble failed")
+		assert(data[2] == _PN532_STARTCODE1, "startcode1 failed")
+		assert(data[3] == _PN532_STARTCODE2, "startcode2 failed")
+		assert(data[4] == length, "length failed")
+		assert(data[5] == ~length + 1, "checksum failed") # @alwin: not too sure about this
+		# data[6] is probably PN532_TO_HOST command
+		# data[7] is probably an echo of the command
+		i = 0
+		while i < length:
+			ret_buf[i] = data[8 + i]
+			i += 1
+		i += 8
+		# data[i] is checksum of data
+		# data[i + 1] is postamble
 
-	return ret_buf
+		return ret_buf
 
+	def read_firmware_version(self):
+		self._pn532_write_command([_PN532_CMD_GETFIRMWAREVERSION], [], _DEFAULT_READ_ACK_FRAME_RETRIES)
+		firmware_version = self.pn532_read_response(DEFAULT_READ_RESPONSE_RETRIES)
+		return list(firmware_version)
 
-print("Beginning PN532 driver")
+	def rf_configuration(self):
+		self._pn532_write_command(_RFCONFIGURATION_HEADER, [], _DEFAULT_READ_ACK_FRAME_RETRIES)
+		self.pn532_read_response(DEFAULT_READ_RESPONSE_RETRIES);
+		return
 
-# Get the i2c device
-i2c_bus = I2C(1)
+	def SAM_configure(self):
+		self._pn532_write_command(_SAM_CONFIGURE_HEADER, [], _DEFAULT_READ_ACK_FRAME_RETRIES)
+		self.pn532_read_response(DEFAULT_READ_RESPONSE_RETRIES);
+		return
 
-print("hello!")
+	def read_uid(self):
+		self._pn532_write_command(_INLISTPASSIVETARGET_HEADER, [], _DEFAULT_READ_ACK_FRAME_RETRIES)
+		buf = self.pn532_read_response(DEFAULT_READ_RESPONSE_RETRIES);
+		if buf[0] != 1:
+			return []
 
-# Getting firmware number
-pn532_write_command([PN532_CMD_GETFIRMWAREVERSION], [], DEFAULT_READ_ACK_FRAME_RETRIES)
-firmware_version = pn532_read_response(DEFAULT_READ_RESPONSE_RETRIES)
-print(list(firmware_version))
+		sens_res = buf[2] << 8
+		sens_res = sens_res | buf[3]
 
+		uid_length = buf[5]
 
+		return buf[6:6+uid_length]
