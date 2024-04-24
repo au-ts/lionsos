96b4235a15f0f2385a92546a835e0c64ee5da562
James Archer <j.archer@unsw.edu.au>
2024-04-24 13:56:33 +1000

    mp: change lwip options to improve tcp performance
    Signed-off-by: James Archer <j.archer@unsw.edu.au>

--- examples/kitty/src/micropython/lwip_include/lwipopts.h
+++ examples/kitty/src/micropython/lwip_include/lwipopts.h
@@ -26,7 +26,7 @@
 #define LWIP_DNS 1
 
 #define MEM_ALIGNMENT 4
-#define MEM_SIZE 0x4000
+#define MEM_SIZE 0x20000
 
 #define ETHARP_SUPPORT_STATIC_ENTRIES 1
 #define SYS_LIGHTWEIGHT_PROT 0
@@ -39,15 +39,23 @@
 #define CHECKSUM_CHECK_ICMP 0
 #define CHECKSUM_CHECK_ICMP6 0
 
-#define TCP_SND_QUEUELEN 2500
-#define MEMP_NUM_TCP_SEG TCP_SND_QUEUELEN
-#define TCP_SND_BUF (100 * TCP_MSS)
-#define TCP_WND (100 * TCP_MSS)
-#define LWIP_WND_SCALE 1
-#define TCP_RCV_SCALE 10
-#define PBUF_POOL_SIZE 1000
-#define MEMP_NUM_SYS_TIMEOUT 512
+#define TCP_MSS 2000 // maximum segment size, max size of a single packet
+#define TCP_WND (50 * TCP_MSS) // tcp window, max data we can receive at once
+#define TCP_SND_BUF (50 * TCP_MSS) // send buffer space
+#define TCP_SND_QUEUELEN 2500 // max queued pbufs
 
+#define TCP_QUEUE_OOSEQ 1 // hold out-of-sequence packets instead of immediately dropping them
+#define LWIP_TCP_SACK_OUT 1 // support sending selective acknowledgements
+
+#define LWIP_WND_SCALE 1 // support window sizes > 65536
+#define TCP_RCV_SCALE 12
+
+#define LWIP_TCP_TIMESTAMPS 1 // support tcp timestamp option
+
+#define PBUF_POOL_SIZE 1000
+#define MEMP_NUM_PBUF TCP_SND_QUEUELEN
+#define MEMP_NUM_TCP_SEG TCP_SND_QUEUELEN
+#define MEMP_NUM_SYS_TIMEOUT 512
 #define MEMP_NUM_TCP_PCB 100
 #define MEMP_NUM_TCP_PCB_LISTEN 100
 #define MEMP_NUM_NETCONN 100
