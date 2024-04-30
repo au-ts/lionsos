4f66890f54237be8c1dd67c2ae96c4246b6cb731
James Archer <j.archer@unsw.edu.au>
2024-04-30 16:41:31 +1000

    fs: use acquire/release operations instead of fences in queue
    Signed-off-by: James Archer <j.archer@unsw.edu.au>

--- fs/protocol/protocol.c
+++ fs/protocol/protocol.c
@@ -4,23 +4,19 @@
 #include <fs/protocol.h>
 
 bool sddf_fs_queue_push(struct sddf_fs_queue *queue, union sddf_fs_message message) {
-    uint64_t tail = queue->tail;
-    if (tail + 1 == queue->head) {
+    if (queue->tail + 1 == __atomic_load_n(&queue->head, __ATOMIC_ACQUIRE)) {
         return false;
     }
-    queue->buffer[tail % SDDF_FS_QUEUE_CAPACITY] = message;
-    __atomic_thread_fence(__ATOMIC_RELEASE);
-    queue->tail = tail + 1;
+    queue->buffer[queue->tail % SDDF_FS_QUEUE_CAPACITY] = message;
+    __atomic_store_n(&queue->tail, queue->tail + 1, __ATOMIC_RELEASE);
     return true;
 }
 
 bool sddf_fs_queue_pop(struct sddf_fs_queue *queue, union sddf_fs_message *message) {
-    uint64_t head = queue->head;
-    if (head == queue->tail) {
+    if (queue->head == __atomic_load_n(&queue->tail, __ATOMIC_ACQUIRE)) {
         return false;
     }
-    *message = queue->buffer[head % SDDF_FS_QUEUE_CAPACITY];
-    __atomic_thread_fence(__ATOMIC_ACQUIRE);
-    queue->head = head + 1;
+    *message = queue->buffer[queue->head % SDDF_FS_QUEUE_CAPACITY];
+    __atomic_store_n(&queue->head, queue->head + 1, __ATOMIC_RELEASE);
     return true;
 }
