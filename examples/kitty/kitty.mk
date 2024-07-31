67e63884747c140dd55a563cd5aae202b1e64edf
Peter Chubb <Peter.Chubb@unsw.edu.au>
2024-08-01 08:39:40 +1000

    A start at Makefile snippets for the Kitty example
    Create the new kitty.mk and fix vmm.c for new paths.
    
    Signed-off-by: Peter Chubb <Peter.Chubb@unsw.edu.au>

---
+++ examples/kitty/kitty.mk
@@ -0,0 +1,199 @@
+#
+# Copyright 2023, UNSW
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+ifeq ($(strip $(MICROKIT_SDK)),)
+$(error MICROKIT_SDK must be specified)
+endif
+MICROKIT_SDK:=$(abspath ${MICROKIT_SDK})
+
+ifeq ($(strip $(LIBGCC)),)
+LIBGCC:=$(dir $(realpath $(shell aarch64-none-elf-gcc --print-file-name libgcc.a)))
+endif
+
+ifeq ($(strip $(LIBMATH)),)
+LIBMATH:=$(dir $(realpath $(shell aarch64-none-elf-gcc --print-file-name libm.a)))
+endif
+
+TOOLCHAIN := clang
+CC := clang
+LD := ld.lld
+RANLIB := llvm-ranlib
+AR := llvm-ar
+TARGET := aarch64-none-elf
+MICROKIT_TOOL ?= $(MICROKIT_SDK)/bin/microkit
+DTC := dtc
+
+BOARD_DIR := $(MICROKIT_SDK)/board/$(MICROKIT_BOARD)/$(MICROKIT_CONFIG)
+PLATFORM := meson
+SDDF := $(LIONSOS)/dep/sddf
+LIBVMM_DIR := $(LIONSOS)/dep/libvmm
+
+VMM_IMAGE_DIR := ${KITTY_DIR}/src/vmm/images
+LINUX := $(VMM_IMAGE_DIR)/linux
+INITRD := $(VMM_IMAGE_DIR)/rootfs.cpio.gz
+DTS := $(VMM_IMAGE_DIR)/linux.dts
+DTB := linux.dtb
+
+LWIP := $(SDDF)/network/ipstacks/lwip/src
+LIBNFS := $(LIONSOS)/dep/libnfs
+NFS := $(LIONSOS)/components/fs/nfs
+MUSL := $(LIONSOS)/dep/musllibc
+#
+# Network config
+CONFIG_INCLUDE ?= ${KITTY_DIR}/src/config
+NUM_NETWORK_CLIENTS := -DNUM_NETWORK_CLIENTS=2
+CFLAGS_network := -I${CONFIG_INCLUDE}
+
+# Serial config
+UART_DRIVER := ${SDDF}/drivers/serial/${PLATFORM}
+SERIAL_NUM_CLIENTS := -DSERIAL_NUM_CLIENTS=1
+
+# I2C config
+I2C_BUS_NUM=2
+
+IMAGES := timer_driver.elf \
+	  vmm.elf \
+	  eth_driver.elf \
+	  micropython.elf \
+	  nfs.elf \
+	  copy.elf \
+	  network_virt_rx.elf \
+	  network_virt_tx.elf \
+	  uart_driver.elf \
+	  serial_rx_virt.elf \
+	  serial_tx_virt.elf \
+	  i2c_virt.elf \
+	  i2c_driver.elf
+
+CFLAGS := \
+	-mtune=$(CPU) \
+	-mstrict-align \
+	-ffreestanding \
+	-g \
+	-O0 \
+	-Wall \
+	-Wno-unused-function \
+	-I$(BOARD_DIR)/include \
+	-target $(TARGET) \
+	-I$(LIBVMM_DIR)/src/arch/aarch64 \
+	-I$(LIBVMM_DIR)/src \
+	-I$(LIBVMM_DIR)/src/util \
+	-DBOARD_$(MICROKIT_BOARD) \
+	-I$(SDDF)/include
+
+LDFLAGS := -L$(BOARD_DIR)/lib
+LIBS := -lmicrokit -Tmicrokit.ld libsddf_util_debug.a
+
+IMAGE_FILE := kitty.img
+REPORT_FILE := report.txt
+
+all: cache.o
+CHECK_FLAGS_BOARD_MD5:=.board_cflags-$(shell echo -- ${CFLAGS} ${BOARD} ${MICROKIT_CONFIG} | shasum | sed 's/ *-//')
+
+${CHECK_FLAGS_BOARD_MD5}:
+	-rm -f .board_cflags-*
+	touch $@
+
+
+%.elf: %.o
+	${LD} ${LDFLAGS} -o $@ $< ${LIBS}
+
+include ${SDDF}/util/util.mk
+include ${LIBVMM_DIR}/vmm.mk
+include ${SDDF}/drivers/clock/${PLATFORM}/timer_driver.mk
+include ${SDDF}/drivers/network/${PLATFORM}/eth_driver.mk
+include ${SDDF}/drivers/i2c/${PLATFORM}/i2c_driver.mk
+include ${SDDF}/drivers/serial/${PLATFORM}/uart_driver.mk
+include ${SDDF}/network/components/network_components.mk
+include ${SDDF}/serial/components/serial_components.mk
+include ${SDDF}/i2c/components/i2c_virt.mk
+include ${SDDF}/libco/libco.mk
+
+# Build the VMM for graphics
+VMM_OBJS := vmm.o package_guest_images.o
+VPATH := ${LIBVMM_DIR}:${VMM_IMAGE_DIR}:${VMM_IMAGE_DIR}/..
+
+$(DTB): $(DTS)
+	$(DTC) -q -I dts -O dtb $< > $@
+
+package_guest_images.o: $(LIBVMM_DIR)/tools/package_guest_images.S \
+			$(VMM_IMAGE_DIR) $(LINUX) $(INITRD) $(DTB)
+	$(CC) -c -g3 -x assembler-with-cpp \
+					-DGUEST_KERNEL_IMAGE_PATH=\"$(LINUX)\" \
+					-DGUEST_DTB_IMAGE_PATH=\"$(DTB)\" \
+					-DGUEST_INITRD_IMAGE_PATH=\"$(INITRD)\" \
+					-target $(TARGET) \
+					$< -o $@
+
+
+vmm.elf: ${VMM_OBJS} libvmm.a
+	$(LD) $(LDFLAGS) $^ $(LIBS) -o $@
+
+# Build the 
+nproc=2
+
+micropython.elf: mpy-cross libsddf_util_debug.a libco.a # libm/libm.a 
+	make  -C $(LIONSOS)/components/micropython -j$(nproc) \
+			MICROKIT_SDK=$(MICROKIT_SDK) \
+			MICROKIT_BOARD=$(MICROKIT_BOARD) \
+			MICROKIT_CONFIG=$(MICROKIT_CONFIG) \
+			MICROPY_MPYCROSS=$(abspath mpy_cross/mpy-cross) \
+			MICROPY_MPYCROSS_DEPENDENCY=$(abspath mpy_cross/mpy-cross) \
+			BUILD=$(abspath .) \
+			LIBMATH=${LIBMATH} \
+			ETHERNET_CONFIG_INCLUDE=$(abspath $(ETHERNET_CONFIG_INCLUDE)) \
+			ENABLE_I2C=1 \
+			ENABLE_FRAMEBUFFER=1 \
+			V=1
+
+musllibc/lib/libc.a:
+	make -C $(MUSL) \
+		C_COMPILER=aarch64-none-elf-gcc \
+		TOOLPREFIX=aarch64-none-elf- \
+		CONFIG_ARCH_AARCH64=y \
+		STAGE_DIR=$(abspath $(BUILD_DIR)/musllibc) \
+		SOURCE_DIR=.
+
+libnfs/lib/libnfs.a: musllibc/lib/libc.a
+	MUSL=$(abspath musllibc) cmake -S $(LIBNFS) -B libnfs
+	cmake --build libnfs
+
+nfs/nfs.a: musllibc/lib/libc.a FORCE
+	make -C $(NFS) \
+		BUILD_DIR=$(abspath $(BUILD_DIR)/nfs) \
+		MICROKIT_INCLUDE=$(BOARD_DIR)/include \
+		MUSLLIBC_INCLUDE=$(abspath musllibc/include) \
+		LIBNFS_INCLUDE=$(abspath $(LIBNFS)/include) \
+		ETHERNET_CONFIG_INCLUDE=$(abspath $(ETHERNET_CONFIG_INCLUDE))
+
+nfs.elf: nfs/nfs.a libnfs/lib/libnfs.a musllibc/lib/libc.a
+	$(LD) \
+		$(LDFLAGS) \
+		nfs/nfs.a \
+		-Llibnfs/lib \
+		-Lmusllibc/lib \
+		-L$(LIBGCC) \
+		-lgcc \
+		-lc \
+		$(LIBS) \
+		-lnfs \
+		-o nfs.elf
+
+${IMAGES}: libsddf_util_debug.a
+
+%.o: %.c
+	${CC} ${CFLAGS} -c -o $@ $<
+
+$(IMAGE_FILE) $(REPORT_FILE): $(IMAGES) ${KITTY_DIR}/kitty.system
+	$(MICROKIT_TOOL) ${KITTY_DIR}/kitty.system --search-path $(BUILD_DIR) --board $(MICROKIT_BOARD) --config $(MICROKIT_CONFIG) -o $(IMAGE_FILE) -r $(REPORT_FILE)
+
+FORCE:
+
+%.elf: %.o
+	${LD} ${LDFLAGS} -o $@ $< ${LIBS}
+
+mpy-cross: FORCE
+	${MAKE} -C ${LIONSOS}/dep/micropython/mpy-cross BUILD=$(abspath ./mpy_cross)
--- examples/kitty/src/vmm/vmm.c
+++ examples/kitty/src/vmm/vmm.c
@@ -6,14 +6,14 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <microkit.h>
-#include "util/util.h"
-#include "arch/aarch64/vgic/vgic.h"
-#include "arch/aarch64/linux.h"
-#include "arch/aarch64/fault.h"
-#include "guest.h"
-#include "virq.h"
-#include "tcb.h"
-#include "vcpu.h"
+#include "libvmm/util/util.h"
+#include "libvmm/arch/aarch64/vgic/vgic.h"
+#include "libvmm/arch/aarch64/linux.h"
+#include "libvmm/arch/aarch64/fault.h"
+#include "libvmm/guest.h"
+#include "libvmm/virq.h"
+#include "libvmm/tcb.h"
+#include "libvmm/vcpu.h"
 /* Specific to the framebuffer example */
 #include "uio.h"
 
