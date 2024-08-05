d08b5c560d1ee2cdcb6428db9afbddf54089e4ea
Courtney Darville <courtneydarville94@outlook.com>
2024-08-05 15:38:33 +1000

    Remove serial rx virt from webserver system
    Signed-off-by: Courtney Darville <courtneydarville94@outlook.com>

--- examples/webserver/config/serial_config.h
+++ examples/webserver/config/serial_config.h
@@ -11,8 +11,8 @@
 /* Number of clients of the serial subsystem. */
 #define SERIAL_NUM_CLIENTS 2
 
-/* Support full duplex. */
-#define SERIAL_TX_ONLY 0
+/* Support only output. */
+#define SERIAL_TX_ONLY 1
 
 /* Associate a colour with each client's output. */
 #define SERIAL_WITH_COLOUR 1
@@ -23,7 +23,6 @@
 /* One read/write client, one write only client */
 #define SERIAL_CLI0_NAME "micropython"
 #define SERIAL_CLI1_NAME "nfs"
-#define SERIAL_VIRT_RX_NAME "serial_virt_rx"
 #define SERIAL_VIRT_TX_NAME "serial_virt_tx"
 
 #define SERIAL_QUEUE_SIZE                          0x1000
@@ -33,22 +32,15 @@
 #define SERIAL_TX_DATA_REGION_SIZE_CLI0            SERIAL_DATA_REGION_SIZE
 #define SERIAL_TX_DATA_REGION_SIZE_CLI1            SERIAL_DATA_REGION_SIZE
 
-#define SERIAL_RX_DATA_REGION_SIZE_DRIV            SERIAL_DATA_REGION_SIZE
-#define SERIAL_RX_DATA_REGION_SIZE_CLI0            SERIAL_DATA_REGION_SIZE
-
 #define SERIAL_MAX_TX_DATA_SIZE MAX(SERIAL_TX_DATA_REGION_SIZE_DRIV, \
                                     MAX(SERIAL_TX_DATA_REGION_SIZE_CLI0, \
                                     SERIAL_TX_DATA_REGION_SIZE_CLI1))
-#define SERIAL_MAX_RX_DATA_SIZE MAX(SERIAL_RX_DATA_REGION_SIZE_DRIV, \
-                                    SERIAL_RX_DATA_REGION_SIZE_CLI0)
-#define SERIAL_MAX_DATA_SIZE MAX(SERIAL_MAX_TX_DATA_SIZE, \
-                                 SERIAL_MAX_RX_DATA_SIZE)
 
 /* String to be printed to start console input */
 #define SERIAL_CONSOLE_BEGIN_STRING ""
 #define SERIAL_CONSOLE_BEGIN_STRING_LEN 0
 
-_Static_assert(SERIAL_MAX_DATA_SIZE < UINT32_MAX,
+_Static_assert(SERIAL_MAX_TX_DATA_SIZE < UINT32_MAX,
                "Data regions must be smaller than UINT32"
                " max to use queue data structure correctly.");
 
@@ -63,8 +55,6 @@ static inline void serial_cli_queue_init_sys(const char *pd_name,
                                              char *tx_data)
 {
     if (!sddf_strcmp(pd_name, SERIAL_CLI0_NAME)) {
-        serial_queue_init(rx_queue_handle, rx_queue,
-                        SERIAL_RX_DATA_REGION_SIZE_CLI0, rx_data);
         serial_queue_init(tx_queue_handle, tx_queue,
                         SERIAL_TX_DATA_REGION_SIZE_CLI0, tx_data);
     } else if (!sddf_strcmp(pd_name, SERIAL_CLI1_NAME)) {
@@ -78,10 +68,7 @@ static inline void serial_virt_queue_init_sys(char *pd_name,
                                               serial_queue_t *cli_queue,
                                               char *cli_data)
 {
-    if (!sddf_strcmp(pd_name, SERIAL_VIRT_RX_NAME)) {
-        serial_queue_init(cli_queue_handle, cli_queue,
-                          SERIAL_RX_DATA_REGION_SIZE_CLI0, cli_data);
-    } else if (!sddf_strcmp(pd_name, SERIAL_VIRT_TX_NAME)) {
+    if (!sddf_strcmp(pd_name, SERIAL_VIRT_TX_NAME)) {
         serial_queue_init(cli_queue_handle, cli_queue,
                           SERIAL_TX_DATA_REGION_SIZE_CLI0, cli_data);
         serial_queue_init(&cli_queue_handle[1],
