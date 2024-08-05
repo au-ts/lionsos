2b397a9eaebc2b9e91fbec2ce2a698cc9a193796
Courtney Darville <courtneydarville94@outlook.com>
2024-08-05 15:38:33 +1000

    Remove serial rx virt from webserver system
    Signed-off-by: Courtney Darville <courtneydarville94@outlook.com>

--- examples/webserver/webserver.mk
+++ examples/webserver/webserver.mk
@@ -29,7 +29,7 @@ MUSL=$(LIONSOS)/dep/musllibc
 
 IMAGES := timer_driver.elf eth_driver.elf micropython.elf nfs.elf \
 	  copy.elf network_virt_rx.elf network_virt_tx.elf \
-	  uart_driver.elf serial_virt_rx.elf serial_virt_tx.elf
+	  uart_driver.elf serial_virt_tx.elf
 
 CFLAGS := \
 	-mtune=$(CPU) \
