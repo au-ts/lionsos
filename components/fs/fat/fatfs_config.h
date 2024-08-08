e5ad8ac8a2b49c5d2af4bb908e064ffa7efbf125
Cheng <lichengchaoreng@gmail.com>
2024-08-08 15:51:43 +1000

    Fatfs cleanup branch based on new fs_protocol
    Signed-off-by: Cheng <lichengchaoreng@gmail.com>

---
+++ components/fs/fat/fatfs_config.h
@@ -0,0 +1,27 @@
+#pragma once
+
+# define FS_DEBUG_PRINT
+
+#define DATA_REGION_SIZE 0x4000000
+
+// Maximum fat partition the file system can have
+#define MAX_FATFS 1
+
+// Maximum opened files
+#define MAX_OPENED_FILENUM 128
+
+// Maximum opened directories
+#define MAX_OPENED_DIRNUM 128
+
+// Coroutine library that is using
+#ifndef USE_FIBERPOOL
+#ifndef USE_LIBMICROKITCO
+#define USE_LIBMICROKITCO
+#endif
+#endif
+
+#define WORKER_COROUTINE_NUM 4
+
+#define COROUTINE_STACKSIZE 0x40000
+
+#define LIBMICROKITCO_MAX_COTHREADS (WORKER_COROUTINE_NUM + 1)
---
+++ components/fs/fat/libmicrokitco_opts.h
@@ -0,0 +1,1 @@
+#include "fatfs_config.h"
