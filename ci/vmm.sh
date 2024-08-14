0b5df4772785790335f1477e96157bb4aa017f31
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-08-14 11:42:02 +1000

    ci: add vmm example
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

---
+++ ci/vmm.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# This script aims to build an already checked out version of LionsOS.
+#
+
+set -e
+
+if [ "$#" -ne 2 ]; then
+    echo "usage: vmm.sh /path/to/lionsos /path/to/microkit/sdk"
+    exit 1
+fi
+
+LIONSOS=$1
+MICROKIT_SDK=$2
+
+BUILD_DIR=$LIONSOS/ci_build/vmm
+rm -rf $BUILD_DIR
+
+export BUILD_DIR=$BUILD_DIR
+export MICROKIT_SDK=$MICROKIT_SDK
+export LIONSOS=$LIONSOS
+
+cd $LIONSOS/examples/vmm
+make submodules
+make
