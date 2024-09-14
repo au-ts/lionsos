64d466099b39c57e79becd5b9f0dc5d0bcadfc8a
Manmeet Dhaliwal <m.dhaliwal@student.unsw.edu.au>
2024-09-14 21:28:46 +1000

    add makefile to build pkt forwarder with copiers

---
+++ examples/firewall/firewall.mk
@@ -0,0 +1,127 @@
+MICROKIT_TOOL ?= $(MICROKIT_SDK)/bin/microkit
+FIREWALL:=${LIONSOS}/examples/firewall
+UTIL:=$(SDDF)/util
+ETHERNET_DRIVER_0:=$(SDDF)/drivers/network/$(DRIV_DIR_0)
+ETHERNET_DRIVER_1:=$(SDDF)/drivers/network/$(DRIV_DIR_1)
+ETHERNET_CONFIG_INCLUDE_0:=${FIREWALL}/include/ethernet_config_imx
+ETHERNET_CONFIG_INCLUDE_1:=${FIREWALL}/include/ethernet_config_starfive
+NETWORK_COMPONENTS:=$(SDDF)/network/components
+
+BOARD_DIR := $(MICROKIT_SDK)/board/$(MICROKIT_BOARD)/$(MICROKIT_CONFIG)
+SYSTEM_FILE := ${FIREWALL}/firewall.system
+IMAGE_FILE := loader.img
+REPORT_FILE := report.txt
+
+vpath %.c ${SDDF} ${FIREWALL}
+
+IMAGES := eth_driver_0.elf eth_driver_1.elf network_virt_rx_0.elf network_virt_rx_1.elf \
+	  network_virt_tx_0.elf network_virt_tx_1.elf copy_0.elf copy_1.elf
+
+CFLAGS := -mcpu=$(CPU) \
+	  -mstrict-align \
+	  -ffreestanding \
+	  -g3 -O3 -Wall \
+	  -Wno-unused-function \
+	  -DMICROKIT_CONFIG_$(MICROKIT_CONFIG) \
+	  -I$(BOARD_DIR)/include \
+	  -I$(SDDF)/include \
+	  -MD \
+	  -MP
+
+LDFLAGS := -L$(BOARD_DIR)/lib -L${LIBC}
+LIBS := --start-group -lmicrokit -Tmicrokit.ld -lc libsddf_util_debug.a --end-group
+
+CHECK_FLAGS_BOARD_MD5:=.board_cflags-$(shell echo -- ${CFLAGS} ${BOARD} ${MICROKIT_CONFIG} | shasum | sed 's/ *-//')
+
+${CHECK_FLAGS_BOARD_MD5}:
+	-rm -f .board_cflags-*
+	touch $@
+
+# %.elf: %.o
+# 	$(LD) $(LDFLAGS) $< $(LIBS) -o $@
+
+all: loader.img
+
+${IMAGES}: libsddf_util_debug.a
+
+${IMAGE_FILE} $(REPORT_FILE): $(IMAGES) $(SYSTEM_FILE)
+	$(MICROKIT_TOOL) $(SYSTEM_FILE) --search-path $(BUILD_DIR) --board $(MICROKIT_BOARD) --config $(MICROKIT_CONFIG) -o $(IMAGE_FILE) -r $(REPORT_FILE)
+
+include ${SDDF}/util/util.mk
+
+# common settings for eth0 and eth1
+CHECK_NETWORK_FLAGS_MD5:=.network_cflags-$(shell echo -- ${CFLAGS} ${CFLAGS_network} | shasum | sed 's/ *-//')
+${CHECK_NETWORK_FLAGS_MD5}:
+	-rm -f .network_cflags-*
+	touch $@
+
+network/components: 
+	mkdir -p network/components
+
+# generate network components for eth0
+NETWORK_IMAGES_0:= network_virt_rx_0.elf network_virt_tx_0.elf copy_0.elf
+${NETWORK_IMAGES_0}: LIBS := libsddf_util_debug.a ${LIBS}
+
+network/components/eth0/%.o: ${SDDF}/network/components/%.c
+	mkdir -p network/components/eth0
+	${CC} ${CFLAGS} -I${ETHERNET_CONFIG_INCLUDE_0} -c -o $@ $<
+
+NETWORK_COMPONENT_OBJ_0 := $(addprefix network/components/eth0/, copy.o network_virt_tx.o network_virt_rx.o)
+
+${NETWORK_COMPONENT_OBJ_0}: |network/components
+${NETWORK_COMPONENT_OBJ_0}: ${CHECK_NETWORK_FLAGS_MD5}
+${NETWORK_COMPONENT_OBJ_0}: CFLAGS+=${CFLAGS_network}
+
+network/components/eth0/network_virt_%.o: ${SDDF}/network/components/virt_%.c 
+	mkdir -p network/components/eth0
+	${CC} ${CFLAGS} -I${ETHERNET_CONFIG_INCLUDE_0} -c -o $@ $<
+
+%_0.elf: network/components/eth0/%.o
+	${LD} ${LDFLAGS} -o $@ $< ${LIBS}
+
+# generate network components for eth1
+NETWORK_IMAGES_1:= network_virt_rx_1.elf network_virt_tx_1.elf copy_1.elf
+${NETWORK_IMAGES_1}: LIBS := libsddf_util_debug.a ${LIBS}
+
+network/components/eth1/%.o: ${SDDF}/network/components/%.c
+	mkdir -p network/components/eth1
+	${CC} ${CFLAGS} -I${ETHERNET_CONFIG_INCLUDE_1} -c -o $@ $<
+
+NETWORK_COMPONENT_OBJ_1 := $(addprefix network/components/eth1/, copy.o network_virt_tx.o network_virt_rx.o)
+
+${NETWORK_COMPONENT_OBJ_1}: |network/components
+${NETWORK_COMPONENT_OBJ_1}: ${CHECK_NETWORK_FLAGS_MD5}
+${NETWORK_COMPONENT_OBJ_1}: CFLAGS+=${CFLAGS_network}
+
+network/components/eth1/network_virt_%.o: ${SDDF}/network/components/virt_%.c 
+	mkdir -p network/components/eth1
+	${CC} ${CFLAGS} -I${ETHERNET_CONFIG_INCLUDE_1} -c -o $@ $<
+
+%_1.elf: network/components/eth1/%.o
+	${LD} ${LDFLAGS} -o $@ $< ${LIBS}
+
+# generate eth0 driver 
+imx/ethernet.o: ${ETHERNET_DRIVER_0}/ethernet.c ${CHECK_NETDRV_FLAGS_MD5}
+	mkdir -p imx
+	${CC} -c ${CFLAGS} ${CFLAGS_network} -I${ETHERNET_CONFIG_INCLUDE_0} -I ${ETHERNET_DRIVER_0} -o $@ $<
+
+eth_driver_0.elf: imx/ethernet.o
+	$(LD) $(LDFLAGS) $< $(LIBS) -o $@
+
+# generate eth1 driver 
+starfive/ethernet.o: ${ETHERNET_DRIVER_1}/ethernet.c ${CHECK_NETDRV_FLAGS}
+	mkdir -p starfive
+	${CC} -c ${CFLAGS} ${CFLAGS_network} -I${ETHERNET_CONFIG_INCLUDE_1} -I ${ETHERNET_DRIVER_1} -o $@ $<
+
+eth_driver_1.elf: starfive/ethernet.o
+	$(LD) $(LDFLAGS) $< $(LIBS) -o $@
+
+clean::
+	${RM} -f *.elf .depend* $
+	find . -name \*.[do] |xargs --no-run-if-empty rm
+
+clobber:: clean
+	rm -f *.a
+	rm -f ${IMAGE_FILE} ${REPORT_FILE}
+
+-include $(DEPS)
---
+++ examples/firewall/include/ethernet_config_starfive/ethernet_config.h
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2024, UNSW
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <microkit.h>
+#include <sddf/util/string.h>
+#include <sddf/network/queue.h>
+#include <sddf/util/util.h>
+
+#define NUM_NETWORK_CLIENTS 1
+
+#define NET_CLI0_NAME "eth0_virt_tx"
+#define NET_COPY0_NAME "eth1_copy"
+#define NET_VIRT_RX_NAME "eth1_virt_rx"
+#define NET_VIRT_TX_NAME "eth1_virt_tx"
+#define NET_DRIVER_NAME "eth1"
+
+#define NET_DATA_REGION_SIZE                    0x200000
+#define NET_HW_REGION_SIZE                      0x10000
+
+#if defined(CONFIG_PLAT_IMX8MP_EVK)
+#define MAC_ADDR_CLI0                       0x52540100000A
+#else
+#error "Must define MAC addresses for clients in ethernet config"
+#endif
+
+#define NET_TX_QUEUE_SIZE_CLI0                   512
+#define NET_TX_QUEUE_SIZE_DRIV                   NET_TX_QUEUE_SIZE_CLI0
+
+#define NET_TX_DATA_REGION_SIZE_CLI0             NET_DATA_REGION_SIZE
+
+_Static_assert(NET_TX_DATA_REGION_SIZE_CLI0 >= NET_TX_QUEUE_SIZE_CLI0 *NET_BUFFER_SIZE,
+               "Client0 TX data region size must fit Client0 TX buffers");
+
+#define NET_RX_QUEUE_SIZE_DRIV                   512
+#define NET_RX_QUEUE_SIZE_CLI0                   512
+#define NET_MAX_CLIENT_QUEUE_SIZE                NET_RX_QUEUE_SIZE_CLI0
+#define NET_RX_QUEUE_SIZE_COPY0                  NET_RX_QUEUE_SIZE_DRIV
+
+#define NET_RX_DATA_REGION_SIZE_DRIV            NET_DATA_REGION_SIZE
+#define NET_RX_DATA_REGION_SIZE_CLI0            NET_DATA_REGION_SIZE
+
+_Static_assert(NET_RX_DATA_REGION_SIZE_DRIV >= NET_RX_QUEUE_SIZE_DRIV *NET_BUFFER_SIZE,
+               "Driver RX data region size must fit Driver RX buffers");
+_Static_assert(NET_RX_DATA_REGION_SIZE_CLI0 >= NET_RX_QUEUE_SIZE_CLI0 *NET_BUFFER_SIZE,
+               "Client0 RX data region size must fit Client0 RX buffers");
+
+#define NET_MAX_QUEUE_SIZE MAX(NET_TX_QUEUE_SIZE_DRIV, MAX(NET_RX_QUEUE_SIZE_DRIV, NET_RX_QUEUE_SIZE_CLI0))
+_Static_assert(NET_TX_QUEUE_SIZE_DRIV >= NET_TX_QUEUE_SIZE_CLI0,
+               "Driver TX queue must have capacity to fit all of client's TX buffers.");
+_Static_assert(NET_RX_QUEUE_SIZE_COPY0 >= NET_RX_QUEUE_SIZE_DRIV,
+               "Copy0 queues must have capacity to fit all RX buffers.");
+_Static_assert(sizeof(net_queue_t) + NET_MAX_QUEUE_SIZE *sizeof(net_buff_desc_t) <= NET_DATA_REGION_SIZE,
+               "net_queue_t must fit into a single data region.");
+
+static inline uint64_t net_cli_mac_addr(char *pd_name)
+{
+    if (!sddf_strcmp(pd_name, NET_CLI0_NAME)) {
+        return MAC_ADDR_CLI0;
+    }
+
+    return 0;
+}
+
+static inline void net_virt_mac_addrs(char *pd_name, uint64_t macs[NUM_NETWORK_CLIENTS])
+{
+    if (!sddf_strcmp(pd_name, NET_VIRT_RX_NAME)) {
+        macs[0] = MAC_ADDR_CLI0;
+    }
+}
+
+static inline void net_cli_queue_size(char *pd_name, size_t *rx_queue_size, size_t *tx_queue_size)
+{
+    if (!sddf_strcmp(pd_name, NET_CLI0_NAME)) {
+        *rx_queue_size = NET_RX_QUEUE_SIZE_CLI0;
+        *tx_queue_size = NET_TX_QUEUE_SIZE_CLI0;
+    }
+}
+
+static inline void net_copy_queue_size(char *pd_name, size_t *cli_queue_size, size_t *virt_queue_size)
+{
+    if (!sddf_strcmp(pd_name, NET_COPY0_NAME)) {
+        *cli_queue_size = NET_RX_QUEUE_SIZE_CLI0;
+        *virt_queue_size = NET_RX_QUEUE_SIZE_COPY0;
+    } 
+}
+
+typedef struct net_queue_info {
+    net_queue_t *free;
+    net_queue_t *active;
+    size_t size;
+} net_queue_info_t;
+
+static inline void net_virt_queue_info(char *pd_name, net_queue_t *cli0_free, net_queue_t *cli0_active,
+                                       net_queue_info_t ret[NUM_NETWORK_CLIENTS])
+{
+    if (!sddf_strcmp(pd_name, NET_VIRT_RX_NAME)) {
+        ret[0] = (net_queue_info_t) {
+            .free = cli0_free, .active = cli0_active, .size = NET_RX_QUEUE_SIZE_COPY0
+        };
+    } else if (!sddf_strcmp(pd_name, NET_VIRT_TX_NAME)) {
+        ret[0] = (net_queue_info_t) {
+            .free = cli0_free, .active = cli0_active, .size = NET_TX_QUEUE_SIZE_CLI0
+        };
+    }
+}
+
+static inline void net_mem_region_vaddr(char *pd_name, uintptr_t mem_regions[NUM_NETWORK_CLIENTS],
+                                        uintptr_t start_region)
+{
+    if (!sddf_strcmp(pd_name, NET_VIRT_TX_NAME)) {
+        mem_regions[0] = start_region;
+    }
+}
