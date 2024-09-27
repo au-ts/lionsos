06e382bd73c73416a15df6e6b4add0ed56ca5773
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-09-27 17:24:07 +1000

    kitty: cleanup/improve pic_gen.py script
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- examples/kitty/client/pic_gen.py
+++ examples/kitty/client/pic_gen.py
@@ -1,28 +1,34 @@
 # Copyright 2024, UNSW
 # SPDX-License-Identifier: BSD-2-Clause
 
+# This script's purpose is to take a PNG and convert it
+# into a binary format that can be read in MicroPython.
+# While not particularly space-efficient, it means that we
+# do not have to parse/read a PNG file in a MicroPython environment,
+
 import png
+import argparse
 
-data = list(png.Reader("catwithfish.png").read()[2])
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--png", required=True)
+    parser.add_argument("--data", required=True)
+    args = parser.parse_args()
 
-with open("catwithfish.data", "wb") as f:
-    print(data)
-    for d in data:
-        f.write(d)
+    data = list(png.Reader(args.png).read()[2])
 
-w, h = len(data[0]) // 4, len(data)
-assert w == 400
-assert h == 430
-print("reading")
-with open("catwithfish.data", "rb") as f:
-    pic = list(f.read())
-    w = 400
-    h = 430
-    for y in range(h):
-        for x in range(w):
-            pic_pixel = pic[(y * w * 4 + (x * 4)):(y * w * 4 + (x * 4))+4]
-            data_pixel = data[y][(x * 4):(x * 4)+4]
-            if bytearray(pic_pixel) != data_pixel:
-                print(bytearray(pic_pixel), data_pixel, x, y)
-            assert bytearray(pic_pixel) == data_pixel
+    with open(args.data, "wb") as f:
+        for d in data:
+            f.write(d)
 
+    w, h = len(data[0]) // 4, len(data)
+    print("INFO: image width: {w}, height: {h}")
+    with open(args.data, "rb") as f:
+        pic = list(f.read())
+        for y in range(h):
+            for x in range(w):
+                pic_pixel = pic[(y * w * 4 + (x * 4)):(y * w * 4 + (x * 4))+4]
+                data_pixel = data[y][(x * 4):(x * 4)+4]
+                if bytearray(pic_pixel) != data_pixel:
+                    print(bytearray(pic_pixel), data_pixel, x, y)
+                assert bytearray(pic_pixel) == data_pixel
