cb92f0c22bc87b243d838efd6de6651d9037af1f
Peter Chubb <Peter.Chubb@unsw.edu.au>
2024-10-09 15:21:15 +1100

    Change size->capacity in kitty example
    Signed-off-by: Peter Chubb <Peter.Chubb@unsw.edu.au>

--- examples/kitty/src/config/serial_config.h
+++ examples/kitty/src/config/serial_config.h
@@ -26,29 +26,29 @@
 #define SERIAL_VIRT_RX_NAME "serial_virt_rx"
 #define SERIAL_VIRT_TX_NAME "serial_virt_tx"
 
-#define SERIAL_QUEUE_SIZE                          0x1000
-#define SERIAL_DATA_REGION_SIZE                    0x2000
+#define SERIAL_QUEUE_CAPACITY                          0x1000
+#define SERIAL_DATA_REGION_CAPACITY                    0x2000
 
-#define SERIAL_TX_DATA_REGION_SIZE_DRIV            (2 * SERIAL_DATA_REGION_SIZE)
-#define SERIAL_TX_DATA_REGION_SIZE_CLI0            SERIAL_DATA_REGION_SIZE
-#define SERIAL_TX_DATA_REGION_SIZE_CLI1            SERIAL_DATA_REGION_SIZE
+#define SERIAL_TX_DATA_REGION_CAPACITY_DRIV            (2 * SERIAL_DATA_REGION_CAPACITY)
+#define SERIAL_TX_DATA_REGION_CAPACITY_CLI0            SERIAL_DATA_REGION_CAPACITY
+#define SERIAL_TX_DATA_REGION_CAPACITY_CLI1            SERIAL_DATA_REGION_CAPACITY
 
-#define SERIAL_RX_DATA_REGION_SIZE_DRIV            SERIAL_DATA_REGION_SIZE
-#define SERIAL_RX_DATA_REGION_SIZE_CLI0            SERIAL_DATA_REGION_SIZE
+#define SERIAL_RX_DATA_REGION_CAPACITY_DRIV            SERIAL_DATA_REGION_CAPACITY
+#define SERIAL_RX_DATA_REGION_CAPACITY_CLI0            SERIAL_DATA_REGION_CAPACITY
 
-#define SERIAL_MAX_TX_DATA_SIZE MAX(SERIAL_TX_DATA_REGION_SIZE_DRIV, \
-                                    MAX(SERIAL_TX_DATA_REGION_SIZE_CLI0, \
-                                    SERIAL_TX_DATA_REGION_SIZE_CLI1))
-#define SERIAL_MAX_RX_DATA_SIZE MAX(SERIAL_RX_DATA_REGION_SIZE_DRIV, \
-                                    SERIAL_RX_DATA_REGION_SIZE_CLI0)
-#define SERIAL_MAX_DATA_SIZE MAX(SERIAL_MAX_TX_DATA_SIZE, \
-                                 SERIAL_MAX_RX_DATA_SIZE)
+#define SERIAL_MAX_TX_DATA_CAPACITY MAX(SERIAL_TX_DATA_REGION_CAPACITY_DRIV, \
+                                    MAX(SERIAL_TX_DATA_REGION_CAPACITY_CLI0, \
+                                    SERIAL_TX_DATA_REGION_CAPACITY_CLI1))
+#define SERIAL_MAX_RX_DATA_CAPACITY MAX(SERIAL_RX_DATA_REGION_CAPACITY_DRIV, \
+                                    SERIAL_RX_DATA_REGION_CAPACITY_CLI0)
+#define SERIAL_MAX_DATA_CAPACITY MAX(SERIAL_MAX_TX_DATA_CAPACITY, \
+                                 SERIAL_MAX_RX_DATA_CAPACITY)
 
 /* String to be printed to start console input */
 #define SERIAL_CONSOLE_BEGIN_STRING ""
 #define SERIAL_CONSOLE_BEGIN_STRING_LEN 0
 
-_Static_assert(SERIAL_MAX_DATA_SIZE < UINT32_MAX,
+_Static_assert(SERIAL_MAX_DATA_CAPACITY < UINT32_MAX,
                "Data regions must be smaller than UINT32"
                " max to use queue data structure correctly.");
 
@@ -64,12 +64,12 @@ static inline void serial_cli_queue_init_sys(const char *pd_name,
 {
     if (!sddf_strcmp(pd_name, SERIAL_CLI0_NAME)) {
         serial_queue_init(rx_queue_handle, rx_queue,
-                        SERIAL_RX_DATA_REGION_SIZE_CLI0, rx_data);
+                        SERIAL_RX_DATA_REGION_CAPACITY_CLI0, rx_data);
         serial_queue_init(tx_queue_handle, tx_queue,
-                        SERIAL_TX_DATA_REGION_SIZE_CLI0, tx_data);
+                        SERIAL_TX_DATA_REGION_CAPACITY_CLI0, tx_data);
     } else if (!sddf_strcmp(pd_name, SERIAL_CLI1_NAME)) {
         serial_queue_init(tx_queue_handle, tx_queue,
-                        SERIAL_TX_DATA_REGION_SIZE_CLI1, tx_data);
+                        SERIAL_TX_DATA_REGION_CAPACITY_CLI1, tx_data);
     }
 }
 
@@ -80,15 +80,15 @@ static inline void serial_virt_queue_init_sys(char *pd_name,
 {
     if (!sddf_strcmp(pd_name, SERIAL_VIRT_RX_NAME)) {
         serial_queue_init(cli_queue_handle, cli_queue,
-                          SERIAL_RX_DATA_REGION_SIZE_CLI0, cli_data);
+                          SERIAL_RX_DATA_REGION_CAPACITY_CLI0, cli_data);
     } else if (!sddf_strcmp(pd_name, SERIAL_VIRT_TX_NAME)) {
         serial_queue_init(cli_queue_handle, cli_queue,
-                          SERIAL_TX_DATA_REGION_SIZE_CLI0, cli_data);
+                          SERIAL_TX_DATA_REGION_CAPACITY_CLI0, cli_data);
         serial_queue_init(&cli_queue_handle[1],
                           (serial_queue_t *)((uintptr_t)cli_queue +
-                                             SERIAL_QUEUE_SIZE),
-                          SERIAL_TX_DATA_REGION_SIZE_CLI1,
-                          cli_data + SERIAL_TX_DATA_REGION_SIZE_CLI0);
+                                             SERIAL_QUEUE_CAPACITY),
+                          SERIAL_TX_DATA_REGION_CAPACITY_CLI1,
+                          cli_data + SERIAL_TX_DATA_REGION_CAPACITY_CLI0);
     }
 }
 
