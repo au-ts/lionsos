20488791b5d311552427764668a249251a6bdf01
Eric Chan <eric.chan2@unsw.edu.au>
2024-10-10 16:23:18 +1100

    Minimising gpu driver vm on qemu, and changing from virtio-gpu-pci to mmio
    Signed-off-by: Eric Chan <eric.chan2@unsw.edu.au>

--- examples/kitty/src/vmm/vmm.c
+++ examples/kitty/src/vmm/vmm.c
@@ -57,7 +57,7 @@ uintptr_t guest_ram_vaddr;
 #if defined(CONFIG_PLAT_ODROIDC4)
 uint32_t irqs[] = { 192, 193, 194, 5, 246, 89, 35 };
 #elif defined(CONFIG_PLAT_QEMU_ARM_VIRT)
-uint32_t irqs[] = { 35, 36, 37, 38 };
+uint32_t irqs[] = { 78 };
 #else
 #error "Need to define platform specific pass-through IRQs"
 #endif
