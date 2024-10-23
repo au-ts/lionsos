d5a54770b3187f6a8e3fc748ea861a8e2379da86
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2024-10-23 15:50:29 +1100

    checkpoint vmm framebuffer
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- examples/kitty/src/vmm/vmm_framebuffer.c
+++ examples/kitty/src/vmm/vmm_framebuffer.c
@@ -15,6 +15,10 @@
 /* Specific to the framebuffer example */
 #include "uio.h"
 
+#include <libvmm/virtio/virtio.h>
+#include <sddf/serial/queue.h>
+#include <serial_config.h>
+
 #if defined(CONFIG_PLAT_ODROIDC4)
 #define GUEST_RAM_SIZE 0x10000000
 #define GUEST_DTB_VADDR 0x2f000000
@@ -62,10 +66,22 @@ uint32_t irqs[] = { 35, 36, 37, 38 };
 #error "Need to define platform specific pass-through IRQs"
 #endif
 
-/* Pinmux handling */
-#define CONFIG_PLAT_ODROIDC4 // @billn remove this, was here to make vscode happy
-#if defined(CONFIG_PLAT_ODROIDC4)
+/* Virtio Console */
+#define SERIAL_VIRT_TX_CH 1
+#define SERIAL_VIRT_RX_CH 2
+#define VIRTIO_CONSOLE_IRQ (74)
+#define VIRTIO_CONSOLE_BASE (0x130000)
+#define VIRTIO_CONSOLE_SIZE (0x1000)
 
+serial_queue_t *serial_rx_queue;
+serial_queue_t *serial_tx_queue;
+
+char *serial_rx_data;
+char *serial_tx_data;
+
+static struct virtio_console_device virtio_console;
+
+/* Pinmux handling */
 #define PINCTRL_DRIVER_CH 2
 
 #include <sddf/pinctrl/board/meson/pinctrl_memranges.h>
@@ -197,8 +213,6 @@ bool bus_vmfault_handler(size_t vcpu_id, uintptr_t addr, size_t fsr, seL4_UserCo
     }
 }
 
-#endif
-
 void uio_gpu_ack(size_t vcpu_id, int irq, void *cookie) {
     // Do nothing, there is no actual IRQ to ack since UIO IRQs are virtual!
 }
@@ -246,7 +260,6 @@ void init(void) {
     virq_register(GUEST_VCPU_ID, UIO_GPU_IRQ, &uio_gpu_ack, NULL);
     fault_register_vm_exception_handler(UIO_INIT_ADDRESS, sizeof(size_t), &uio_init_handler, NULL);
 
-#if defined(CONFIG_PLAT_ODROIDC4)
     bool bus2_fault_reg_ok = fault_register_vm_exception_handler(bus2_vaddr, BUS2_MR_SIZE, &bus_vmfault_handler, (void *) bus2_vaddr);
     if (!bus2_fault_reg_ok) {
         LOG_VMM_ERR("Failed to register the VM fault handler for bus2\n");
@@ -258,7 +271,18 @@ void init(void) {
         LOG_VMM_ERR("Failed to register the VM fault handler for peripherals pinmux\n");
         return;
     }
-#endif
+
+    /* Initialise our sDDF ring buffers for the serial device */
+    serial_queue_handle_t serial_rxq, serial_txq;
+    serial_cli_queue_init_sys(microkit_name, &serial_rxq, serial_rx_queue, serial_rx_data, &serial_txq, serial_tx_queue, serial_tx_data);
+
+    /* Initialise virtIO console device */
+    success = virtio_mmio_console_init(&virtio_console,
+                                  VIRTIO_CONSOLE_BASE,
+                                  VIRTIO_CONSOLE_SIZE,
+                                  VIRTIO_CONSOLE_IRQ,
+                                  &serial_rxq, &serial_txq,
+                                  SERIAL_VIRT_TX_CH);
 
     /* Finally start the guest */
     guest_start(GUEST_VCPU_ID, kernel_pc, GUEST_DTB_VADDR, GUEST_INIT_RAM_DISK_VADDR);
@@ -266,6 +290,12 @@ void init(void) {
 
 void notified(microkit_channel ch) {
     switch (ch) {
+        case SERIAL_VIRT_RX_CH: {
+            /* We have received an event from the serial virtualiser, so we
+            * call the virtIO console handling */
+            virtio_console_handle_rx(&virtio_console);
+            break;
+        }
         case MICROPYTHON_CH: {
             bool success = virq_inject(GUEST_VCPU_ID, UIO_GPU_IRQ);
             if (!success) {
