98ce3990704344f58c00cfebee4123a7902b35ff
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2025-01-17 16:51:58 +1100

    Fixes for webserver
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

--- examples/webserver/meta.py
+++ examples/webserver/meta.py
@@ -2,7 +2,9 @@
 # SPDX-License-Identifier: BSD-2-Clause
 import argparse
 import struct
-from typing import Dict, List, Any, Tuple
+import random
+from dataclasses import dataclass
+from typing import List, Tuple
 from sdfgen import SystemDescription, Sddf, DeviceTree, LionsOs
 
 ProtectionDomain = SystemDescription.ProtectionDomain
@@ -10,36 +12,41 @@ MemoryRegion = SystemDescription.MemoryRegion
 Map = SystemDescription.Map
 Channel = SystemDescription.Channel
 
-class Platform:
-    def __init__(self, name: str, arch: SystemDescription.Arch, paddr_top: int, serial: str, timer: str, ethernet: str):
-        self.name = name
-        self.arch = arch
-        self.paddr_top = paddr_top
-        self.serial = serial
-        self.timer = timer
-        self.ethernet = ethernet
+@dataclass
+class Board:
+    name: str
+    arch: SystemDescription.Arch
+    paddr_top: int
+    serial: str
+    timer: str
+    ethernet: str
 
-# TODO: add QEMU virt RISC-V, i.MX8MM-EVK, i.MX8MQ-EVK, i.MX8MP-EVK
-PLATFORMS: List[Platform] = [
-    Platform("qemu_virt_aarch64", SystemDescription.Arch.AARCH64, 0x6_0000_000, "pl011@9000000", "timer", "virtio_mmio@a003e00"),
-    Platform("odroidc4", SystemDescription.Arch.AARCH64, 0x60000000, "soc/bus@ff800000/serial@3000", "soc/bus@ffd00000/watchdog@f0d0", "soc/ethernet@ff3f0000"),
-    Platform("maaxboard", SystemDescription.Arch.AARCH64, 0x70000000, "soc@0/bus@30800000/serial@30860000", "soc@0/bus@30000000/timer@302d0000", "soc@0/bus@30800000/ethernet@30be0000"),
+
+BOARDS: List[Board] = [
+    Board(
+        name="qemu_virt_aarch64",
+        arch=SystemDescription.Arch.AARCH64,
+        paddr_top=0x6_0000_000,
+        serial="pl011@9000000",
+        timer="timer",
+        ethernet="virtio_mmio@a003e00"
+    ),
 ]
 
 def generate(sdf_file: str, output_dir: str, dtb: DeviceTree):
-    uart_node = dtb.node(platform.serial)
-    assert uart_node is not None
-    ethernet_node = dtb.node(platform.ethernet)
+    serial_node = dtb.node(board.serial)
+    assert serial_node is not None
+    ethernet_node = dtb.node(board.ethernet)
     assert ethernet_node is not None
-    timer_node = dtb.node(platform.timer)
-    assert uart_node is not None
+    timer_node = dtb.node(board.timer)
+    assert timer_node is not None
 
     timer_driver = ProtectionDomain("timer_driver", "timer_driver.elf", priority=101)
     timer_system = Sddf.Timer(sdf, timer_node, timer_driver)
 
     uart_driver = ProtectionDomain("uart_driver", "uart_driver.elf", priority=100)
     serial_virt_tx = ProtectionDomain("serial_virt_tx", "serial_virt_tx.elf", priority=99)
-    serial_system = Sddf.Serial(sdf, uart_node, uart_driver, serial_virt_tx)
+    serial_system = Sddf.Serial(sdf, serial_node, uart_driver, serial_virt_tx)
 
     ethernet_driver = ProtectionDomain("ethernet_driver", "eth_driver.elf", priority=101, budget=100, period=400)
     net_virt_tx = ProtectionDomain("net_virt_tx", "network_virt_tx.elf", priority=100, budget=20000)
@@ -87,20 +94,20 @@ def generate(sdf_file: str, output_dir: str, dtb: DeviceTree):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dtbs", required=True)
+    parser.add_argument("--dtb", required=True)
     parser.add_argument("--sddf", required=True)
-    parser.add_argument("--platform", required=True, choices=[p.name for p in PLATFORMS])
+    parser.add_argument("--board", required=True, choices=[b.name for b in BOARDS])
     parser.add_argument("--output", required=True)
     parser.add_argument("--sdf", required=True)
 
     args = parser.parse_args()
 
-    platform = next(filter(lambda p: p.name == args.platform, PLATFORMS))
+    board = next(filter(lambda b: b.name == args.board, BOARDS))
 
-    sdf = SystemDescription(platform.arch, platform.paddr_top)
+    sdf = SystemDescription(board.arch, board.paddr_top)
     sddf = Sddf(args.sddf)
 
-    with open(args.dtbs + f"/{platform.name}.dtb", "rb") as f:
+    with open(args.dtb, "rb") as f:
         dtb = DeviceTree(f.read())
 
     generate(args.sdf, args.output, dtb)
