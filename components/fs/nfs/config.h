4813496945f4af215e62ee7efc45e364c293e621
James Archer <j.archer@unsw.edu.au>
2025-01-20 15:12:09 +1100

    nfs: use config resource for configuration
    Signed-off-by: James Archer <j.archer@unsw.edu.au>
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

---
+++ components/fs/nfs/config.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2025, UNSW
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#define LIONS_NFS_SERVER_URL_LEN_MAX 4096
+#define LIONS_NFS_EXPORT_PATH_LEN_MAX 4096
+
+#define LIONS_NFS_MAGIC_LEN 8
+static char LIONS_NFS_MAGIC[LIONS_NFS_MAGIC_LEN] = { 'L', 'i', 'o', 'n', 's', 'O', 'S', 0x1 };
+
+typedef struct nfs_config {
+    char magic[LIONS_NFS_MAGIC_LEN];
+    char server[LIONS_NFS_SERVER_URL_LEN_MAX];
+    char export[LIONS_NFS_EXPORT_PATH_LEN_MAX];
+} nfs_config_t;
+
+static bool nfs_config_check_magic(nfs_config_t *config)
+{
+    for (int i = 0; i < LIONS_NFS_MAGIC_LEN; i++) {
+        if (config->magic[i] != LIONS_NFS_MAGIC[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
