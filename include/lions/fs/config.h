f02823c7a753b303952b6835ebb2a7c144f1a230
James Archer <j.archer@unsw.edu.au>
2025-01-20 17:22:48 +1100

    webserver, nfs, micropython: use fs config structs
    Signed-off-by: James Archer <j.archer@unsw.edu.au>
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

---
+++ include/lions/fs/config.h
@@ -0,0 +1,37 @@
+#include <microkit.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <sddf/resources/common.h>
+
+#define LIONS_FS_MAGIC_LEN 8
+static char LIONS_FS_MAGIC[LIONS_FS_MAGIC_LEN] = { 'L', 'i', 'o', 'n', 's', 'O', 'S', 0x1 };
+
+typedef struct fs_connection_resource {
+    region_resource_t command_queue;
+    region_resource_t completion_queue;
+    region_resource_t share;
+    uint16_t queue_len;
+    uint8_t id;
+} fs_connection_resource_t;
+
+typedef struct fs_server_config {
+    char magic[LIONS_FS_MAGIC_LEN];
+    fs_connection_resource_t client;
+} fs_server_config_t;
+
+typedef struct fs_client_config {
+    char magic[LIONS_FS_MAGIC_LEN];
+    fs_connection_resource_t server;
+} fs_client_config_t;
+
+static bool fs_config_check_magic(void *config)
+{
+    char *magic = (char *)config;
+    for (int i = 0; i < LIONS_FS_MAGIC_LEN; i++) {
+        if (magic[i] != LIONS_FS_MAGIC[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
