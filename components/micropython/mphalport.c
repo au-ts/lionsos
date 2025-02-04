84092d8389473c377756f66ab20905d0f6bbc5a9
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2025-02-04 15:20:16 +1100

    micropython: fix for latest sDDF serial changes
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

--- components/micropython/mphalport.c
+++ components/micropython/mphalport.c
@@ -30,12 +30,10 @@ int mp_hal_stdin_rx_chr(void) {
     }
 
     // Dequeue buffer and return char
-    int ret = serial_dequeue(&serial_rx_queue_handle,
+    int ret = serial_dequeue_local(&serial_rx_queue_handle,
                              &serial_rx_queue_handle.queue->head,
                              &c);
     assert(!ret);
-
-    serial_request_producer_signal(&serial_rx_queue_handle);
 
     return c;
 }
@@ -46,8 +44,7 @@ void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len)
 {
     for (;;) {
         uint32_t n = serial_enqueue_batch(&serial_tx_queue_handle, len, str);
-        if (n != 0 && serial_require_producer_signal(&serial_tx_queue_handle)) {
-            serial_cancel_producer_signal(&serial_tx_queue_handle);
+        if (n != 0) {
             microkit_notify(serial_config.tx.id);
         }
         len -= n;
