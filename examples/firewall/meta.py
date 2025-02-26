790fccca0c4cae40508bf077d14540f54dd878f6
James Archer <j.archer@unsw.edu.au>
2025-02-26 15:53:53 +1100

    examples/firewall: add firewall web ui example system
    Signed-off-by: James Archer <j.archer@unsw.edu.au>

---
+++ examples/firewall/meta.py
@@ -0,0 +1,123 @@
+# Copyright 2025, UNSW
+# SPDX-License-Identifier: BSD-2-Clause
+import argparse
+import struct
+from random import randint
+from dataclasses import dataclass
+from typing import List, Tuple
+from sdfgen import SystemDescription, Sddf, DeviceTree, LionsOs
+
+ProtectionDomain = SystemDescription.ProtectionDomain
+MemoryRegion = SystemDescription.MemoryRegion
+Map = SystemDescription.Map
+Channel = SystemDescription.Channel
+
+@dataclass
+class Board:
+    name: str
+    arch: SystemDescription.Arch
+    paddr_top: int
+    serial: str
+    timer: str
+    ethernet: str
+
+
+BOARDS: List[Board] = [
+    Board(
+        name="qemu_virt_aarch64",
+        arch=SystemDescription.Arch.AARCH64,
+        paddr_top=0x6_0000_000,
+        serial="pl011@9000000",
+        timer="timer",
+        ethernet="virtio_mmio@a003e00"
+    ),
+    Board(
+        name="odroidc4",
+        arch=SystemDescription.Arch.AARCH64,
+        paddr_top=0x60000000,
+        serial="soc/bus@ff800000/serial@3000",
+        timer="soc/bus@ffd00000/watchdog@f0d0",
+        ethernet="soc/ethernet@ff3f0000"
+    ),
+    Board(
+        name="maaxboard",
+        arch=SystemDescription.Arch.AARCH64,
+        paddr_top=0x70000000,
+        serial="soc@0/bus@30800000/serial@30860000",
+        timer="soc@0/bus@30000000/timer@302d0000",
+        ethernet="soc@0/bus@30800000/ethernet@30be0000"
+    )
+]
+
+
+def generate(sdf_file: str, output_dir: str, dtb: DeviceTree):
+    serial_node = dtb.node(board.serial)
+    assert serial_node is not None
+    ethernet_node = dtb.node(board.ethernet)
+    assert ethernet_node is not None
+    timer_node = dtb.node(board.timer)
+    assert timer_node is not None
+
+    timer_driver = ProtectionDomain("timer_driver", "timer_driver.elf", priority=101)
+    timer_system = Sddf.Timer(sdf, timer_node, timer_driver)
+
+    serial_driver = ProtectionDomain("serial_driver", "serial_driver.elf", priority=100)
+    serial_virt_tx = ProtectionDomain("serial_virt_tx", "serial_virt_tx.elf", priority=99)
+    serial_system = Sddf.Serial(sdf, serial_node, serial_driver, serial_virt_tx)
+
+    ethernet_driver = ProtectionDomain("ethernet_driver", "eth_driver.elf", priority=101, budget=100, period=400)
+    net_virt_tx = ProtectionDomain("net_virt_tx", "network_virt_tx.elf", priority=100, budget=20000)
+    net_virt_rx = ProtectionDomain("net_virt_rx", "network_virt_rx.elf", priority=99)
+    net_system = Sddf.Net(sdf, ethernet_node, ethernet_driver, net_virt_tx, net_virt_rx)
+
+    micropython = ProtectionDomain("micropython", "micropython.elf", priority=1, budget=20000)
+    micropython_net_copier = ProtectionDomain("micropython_net_copier", "network_copy_micropython.elf", priority=97, budget=20000)
+
+    serial_system.add_client(micropython)
+    timer_system.add_client(micropython)
+    micropython_mac_addr = f"52:54:01:00:00:{hex(randint(0, 0xfe))[2:]:0>2}"
+    net_system.add_client_with_copier(micropython, micropython_net_copier, mac_addr=micropython_mac_addr)
+
+    pds = [
+        serial_driver,
+        serial_virt_tx,
+        ethernet_driver,
+        net_virt_tx,
+        net_virt_rx,
+        micropython,
+        micropython_net_copier,
+        timer_driver,
+    ]
+    for pd in pds:
+        sdf.add_pd(pd)
+
+    assert serial_system.connect()
+    assert serial_system.serialise_config(output_dir)
+    assert net_system.connect()
+    assert net_system.serialise_config(output_dir)
+    assert timer_system.connect()
+    assert timer_system.serialise_config(output_dir)
+
+    with open(f"{output_dir}/{sdf_file}", "w+") as f:
+        f.write(sdf.render())
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dtb", required=True)
+    parser.add_argument("--sddf", required=True)
+    parser.add_argument("--board", required=True, choices=[b.name for b in BOARDS])
+    parser.add_argument("--output", required=True)
+    parser.add_argument("--sdf", required=True)
+
+    args = parser.parse_args()
+
+    board = next(filter(lambda b: b.name == args.board, BOARDS))
+
+    sdf = SystemDescription(board.arch, board.paddr_top)
+    sddf = Sddf(args.sddf)
+
+    with open(args.dtb, "rb") as f:
+        dtb = DeviceTree(f.read())
+
+    generate(args.sdf, args.output, dtb)
