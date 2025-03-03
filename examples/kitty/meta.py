dbfe53e77b49a119ef7b97dabf2757430e463725
James Archer <j.archer@unsw.edu.au>
2025-03-03 16:35:57 +1100

    nfs: use lib_sddf_lwip
    Co-authored-by: Courtney Darville <courtneydarville94@outlook.com>
    Co-authored-by: James Archer <j.archer@unsw.edu.au>
    Signed-off-by: James Archer <j.archer@unsw.edu.au>

--- examples/kitty/meta.py
+++ examples/kitty/meta.py
@@ -112,6 +112,7 @@ def generate(sdf_path: str, output_dir: str, dtb: DeviceTree):
         server=args.nfs_server,
         export_path=args.nfs_dir,
     )
+    nfs_lib_sddf_lwip = Sddf.Lwip(sdf, net_system, nfs)
 
     vmm = ProtectionDomain("framebuffer_vmm", "vmm.elf", priority=1)
     vm = VirtualMachine("linux", [VirtualMachine.Vcpu(id=0)])
@@ -223,6 +224,8 @@ def generate(sdf_path: str, output_dir: str, dtb: DeviceTree):
         assert i2c_system.serialise_config(output_dir)
     assert vmm_system.connect()
     assert vmm_system.serialise_config(output_dir)
+    assert nfs_lib_sddf_lwip.connect()
+    assert nfs_lib_sddf_lwip.serialise_config(output_dir)
 
     with open(f"{output_dir}/{sdf_path}", "w+") as f:
         f.write(sdf.render())
