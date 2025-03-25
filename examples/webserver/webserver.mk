ce928536acfb27d7ff41c05f6f4b99ca2b9e63f4
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2025-03-25 13:49:37 +1100

    examples/webserver: update and convert to metaprogram
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

--- examples/webserver/webserver.mk
+++ examples/webserver/webserver.mk
@@ -12,12 +12,17 @@ SDDF := $(LIONSOS)/dep/sddf
 ifeq (${MICROKIT_BOARD},odroidc4)
 	TIMER_DRIVER_DIR := meson
 	ETHERNET_DRIVER_DIR := meson
-	UART_DRIVER_DIR := meson
+	SERIAL_DRIVER_DIR := meson
 	CPU := cortex-a55
+else ifeq (${MICROKIT_BOARD},maaxboard)
+	TIMER_DRIVER_DIR := imx
+	ETHERNET_DRIVER_DIR := imx
+	SERIAL_DRIVER_DIR := imx
+	CPU := cortex-a53
 else ifeq (${MICROKIT_BOARD},qemu_virt_aarch64)
 	TIMER_DRIVER_DIR := arm
 	ETHERNET_DRIVER_DIR := virtio
-	UART_DRIVER_DIR := arm
+	SERIAL_DRIVER_DIR := arm
 	CPU := cortex-a53
 	QEMU := qemu-system-aarch64
 else
@@ -29,19 +34,26 @@ CC := clang
 LD := ld.lld
 AR := llvm-ar
 RANLIB := llvm-ranlib
+OBJCOPY := llvm-objcopy
 TARGET := aarch64-none-elf
 MICROKIT_TOOL ?= $(MICROKIT_SDK)/bin/microkit
+PYTHON ?= python3
+DTC := dtc
 
 NFS=$(LIONSOS)/components/fs/nfs
 MUSL_SRC := $(LIONSOS)/dep/musllibc
 MUSL := musllibc
 MICRODOT := ${LIONSOS}/dep/microdot/src
 
-IMAGES := timer_driver.elf eth_driver.elf micropython.elf nfs.elf \
-	  copy.elf network_virt_rx.elf network_virt_tx.elf \
-	  uart_driver.elf serial_virt_tx.elf
+METAPROGRAM := $(WEBSERVER_SRC_DIR)/meta.py
+DTS := $(SDDF)/dts/$(MICROKIT_BOARD).dts
+DTB := $(MICROKIT_BOARD).dtb
 
-SYSTEM_FILE := $(WEBSERVER_SRC_DIR)/board/$(MICROKIT_BOARD)/webserver.system
+IMAGES := timer_driver.elf eth_driver.elf micropython.elf nfs.elf \
+	  network_copy.elf network_virt_rx.elf network_virt_tx.elf \
+	  serial_driver.elf serial_virt_tx.elf
+
+SYSTEM_FILE := webserver.system
 
 CFLAGS := \
 	-mtune=$(CPU) \
@@ -57,7 +69,7 @@ CFLAGS := \
 	-DBOARD_$(MICROKIT_BOARD) \
 	-I$(LIONSOS)/include \
 	-I$(SDDF)/include \
-	-I$(CONFIG_INCLUDE)
+	-I$(SDDF)/include/microkit
 
 LDFLAGS := -L$(BOARD_DIR)/lib
 LIBS := -lmicrokit -Tmicrokit.ld libsddf_util_debug.a
@@ -102,12 +114,37 @@ $(MUSL)/lib/libc.a $(MUSL)/include: ${MUSL_SRC}/Makefile ${MUSL}
 SDDF_MAKEFILES := ${SDDF}/util/util.mk \
 		  ${SDDF}/drivers/timer/${TIMER_DRIVER_DIR}/timer_driver.mk \
 		  ${SDDF}/drivers/network/${ETHERNET_DRIVER_DIR}/eth_driver.mk \
-		  ${SDDF}/drivers/serial/${UART_DRIVER_DIR}/uart_driver.mk \
+		  ${SDDF}/drivers/serial/${SERIAL_DRIVER_DIR}/serial_driver.mk \
 		  ${SDDF}/network/components/network_components.mk \
 		  ${SDDF}/serial/components/serial_components.mk
 
 include ${SDDF_MAKEFILES}
 include $(NFS)/nfs.mk
+
+$(DTB): $(DTS)
+	$(DTC) -q -I dts -O dtb $(DTS) > $(DTB)
+
+$(SYSTEM_FILE): $(METAPROGRAM) $(IMAGES) $(DTB)
+	$(PYTHON) $(METAPROGRAM) --sddf $(SDDF) --board $(MICROKIT_BOARD) --dtb $(DTB) --output . --sdf $(SYSTEM_FILE) --nfs-server $(NFS_SERVER) --nfs-dir $(NFS_DIRECTORY)
+	$(OBJCOPY) --update-section .device_resources=serial_driver_device_resources.data serial_driver.elf
+	$(OBJCOPY) --update-section .serial_driver_config=serial_driver_config.data serial_driver.elf
+	$(OBJCOPY) --update-section .serial_virt_tx_config=serial_virt_tx.data serial_virt_tx.elf
+	$(OBJCOPY) --update-section .device_resources=ethernet_driver_device_resources.data eth_driver.elf
+	$(OBJCOPY) --update-section .net_driver_config=net_driver.data eth_driver.elf
+	$(OBJCOPY) --update-section .net_virt_rx_config=net_virt_rx.data network_virt_rx.elf
+	$(OBJCOPY) --update-section .net_virt_tx_config=net_virt_tx.data network_virt_tx.elf
+	$(OBJCOPY) --update-section .net_copy_config=net_copy_micropython_net_copier.data network_copy.elf network_copy_micropython.elf
+	$(OBJCOPY) --update-section .net_copy_config=net_copy_nfs_net_copier.data network_copy.elf network_copy_nfs.elf
+	$(OBJCOPY) --update-section .device_resources=timer_driver_device_resources.data timer_driver.elf
+	$(OBJCOPY) --update-section .timer_client_config=timer_client_micropython.data micropython.elf
+	$(OBJCOPY) --update-section .net_client_config=net_client_micropython.data micropython.elf
+	$(OBJCOPY) --update-section .serial_client_config=serial_client_micropython.data micropython.elf
+	$(OBJCOPY) --update-section .net_client_config=net_client_nfs.data nfs.elf
+	$(OBJCOPY) --update-section .timer_client_config=timer_client_nfs.data nfs.elf
+	$(OBJCOPY) --update-section .serial_client_config=serial_client_nfs.data nfs.elf
+	$(OBJCOPY) --update-section .fs_server_config=fs_server_nfs.data nfs.elf
+	$(OBJCOPY) --update-section .fs_client_config=fs_client_micropython.data micropython.elf
+	$(OBJCOPY) --update-section .nfs_config=nfs_config.data nfs.elf
 
 $(IMAGE_FILE) $(REPORT_FILE): $(IMAGES) $(SYSTEM_FILE)
 	$(MICROKIT_TOOL) $(SYSTEM_FILE) --search-path $(BUILD_DIR) --board $(MICROKIT_BOARD) --config $(MICROKIT_CONFIG) -o $(IMAGE_FILE) -r $(REPORT_FILE)
