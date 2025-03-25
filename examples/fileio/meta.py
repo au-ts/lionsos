79c6be53ee3274ed4fb1eab330e835ffe9267a84
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2025-03-25 13:49:05 +1100

    examples/fileio: update and convert to metaprogram
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

---
+++ examples/fileio/meta.py
@@ -0,0 +1,130 @@
+# Copyright 2025, UNSW
+# SPDX-License-Identifier: BSD-2-Clause
+import argparse
+import struct
+from random import randint
+from dataclasses import dataclass
+from typing import List, Tuple
+from sdfgen import SystemDescription, Sddf, DeviceTree, LionsOs
+
+ProtectionDomain = SystemDescription.ProtectionDomain
+MemoryRegion = SystemDescription.MemoryRegion
+Map = SystemDescription.Map
+Channel = SystemDescription.Channel
+
+@dataclass
+class Board:
+    name: str
+    arch: SystemDescription.Arch
+    paddr_top: int
+    serial: str
+    timer: str
+    blk: str
+    blk_partition: int
+
+
+BOARDS: List[Board] = [
+    Board(
+        name="qemu_virt_aarch64",
+        arch=SystemDescription.Arch.AARCH64,
+        paddr_top=0x6_0000_000,
+        serial="pl011@9000000",
+        timer="timer",
+        blk="virtio_mmio@a003e00",
+        blk_partition=0,
+    ),
+    Board(
+        name="maaxboard",
+        arch=SystemDescription.Arch.AARCH64,
+        paddr_top=0x7_0000_000,
+        serial="soc@0/bus@30800000/serial@30860000",
+        timer="soc@0/bus@30000000/timer@302d0000",
+        blk="soc@0/bus@30800000/mmc@30b40000",
+        blk_partition=3,
+    ),
+]
+
+
+def generate(sdf_path: str, output_dir: str, dtb: DeviceTree):
+    serial_node = dtb.node(board.serial)
+    assert serial_node is not None
+    blk_node = dtb.node(board.blk)
+    assert blk_node is not None
+    timer_node = dtb.node(board.timer)
+    assert timer_node is not None
+
+    timer_driver = ProtectionDomain("timer_driver", "timer_driver.elf", priority=254)
+    timer_system = Sddf.Timer(sdf, timer_node, timer_driver)
+
+    serial_driver = ProtectionDomain("serial_driver", "serial_driver.elf", priority=100)
+    serial_virt_tx = ProtectionDomain("serial_virt_tx", "serial_virt_tx.elf", priority=99)
+    serial_virt_rx = ProtectionDomain("serial_virt_rx", "serial_virt_rx.elf", priority=99)
+    serial_system = Sddf.Serial(sdf, serial_node, serial_driver, serial_virt_tx, virt_rx=serial_virt_rx)
+
+    blk_driver = ProtectionDomain("blk_driver", "blk_driver.elf", priority=200)
+    blk_virt = ProtectionDomain("blk_virt", "blk_virt.elf", priority=199, stack_size=0x2000)
+    blk_system = Sddf.Blk(sdf, blk_node, blk_driver, blk_virt)
+
+    micropython = ProtectionDomain("micropython", "micropython.elf", priority=1)
+
+    serial_system.add_client(micropython)
+    timer_system.add_client(micropython)
+
+    fatfs = ProtectionDomain("fatfs", "fat.elf", priority=96)
+
+    fs = LionsOs.FileSystem.Fat(
+        sdf,
+        fatfs,
+        micropython,
+        blk=blk_system,
+        partition=board.blk_partition
+    )
+
+    if board.name == "maaxboard":
+        timer_system.add_client(blk_driver)
+
+    pds = [
+        serial_driver,
+        serial_virt_tx,
+        serial_virt_rx,
+        micropython,
+        fatfs,
+        timer_driver,
+        blk_driver,
+        blk_virt,
+    ]
+    for pd in pds:
+        sdf.add_pd(pd)
+
+    assert fs.connect()
+    assert fs.serialise_config(output_dir)
+    assert serial_system.connect()
+    assert serial_system.serialise_config(output_dir)
+    assert timer_system.connect()
+    assert timer_system.serialise_config(output_dir)
+    assert blk_system.connect()
+    assert blk_system.serialise_config(output_dir)
+
+    with open(f"{output_dir}/{sdf_path}", "w+") as f:
+        f.write(sdf.render())
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dtb", required=True)
+    parser.add_argument("--sddf", required=True)
+    parser.add_argument("--board", required=True, choices=[b.name for b in BOARDS])
+    parser.add_argument("--output", required=True)
+    parser.add_argument("--sdf", required=True)
+
+    args = parser.parse_args()
+
+    board = next(filter(lambda b: b.name == args.board, BOARDS))
+
+    sdf = SystemDescription(board.arch, board.paddr_top)
+    sddf = Sddf(args.sddf)
+
+    with open(args.dtb, "rb") as f:
+        dtb = DeviceTree(f.read())
+
+    generate(args.sdf, args.output, dtb)
