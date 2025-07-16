f748d575becda377cd075cedd7a4680548baf4bc
Callum <callum.berry76@gmail.com>
2025-07-16 16:48:33 +1000

    Renamed the index to shift everything to the right of left 1 to index_to_delete
    Signed-off-by: Callum <callum.berry76@gmail.com>

--- include/lions/firewall/array_functions.h
+++ include/lions/firewall/array_functions.h
@@ -4,11 +4,11 @@
 #include <sddf/util/util.h>
 #include <string.h>
 
-static void generic_array_shift(void* array, uint32_t entry_size, uint32_t array_len, uint32_t start_point) {
+static void generic_array_shift(void* array, uint32_t entry_size, uint32_t array_len, uint32_t index_to_delete) {
     unsigned char* arr = (unsigned char*) array;
-    uint32_t len = (array_len - start_point - 1) * entry_size;
+    uint32_t len = (array_len - index_to_delete - 1) * entry_size;
     if (len > 0) {
-        uint32_t byte_offset = start_point * entry_size;
+        uint32_t byte_offset = index_to_delete * entry_size;
         memmove(arr + byte_offset, arr + byte_offset + entry_size, len);
     }
 }
