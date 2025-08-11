9b43c8f3806f88e90bd16bc03f97447ed9464f65
Courtney Darville <courtneydarville94@outlook.com>
2025-08-11 15:41:36 +1000

    Update NFS and MP lwipopts.h
    Signed-off-by: Courtney Darville <courtneydarville94@outlook.com>

--- components/micropython/lwip_include/lwipopts.h
+++ components/micropython/lwip_include/lwipopts.h
@@ -1,61 +1,216 @@
 /*
- * Copyright 2020, Data61
- * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
- * ABN 41 687 119 230.
+ * Copyright 2022, UNSW
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 #pragma once
 
 #include <stdlib.h>
+#include <stdbool.h>
+#include <sddf/network/constants.h>
 
+/**
+ * Use lwIP without OS-awareness (no thread, semaphores, mutexes or mboxes).
+ */
 #define NO_SYS 1
+
+/**
+ * Drop support for sys_timeout and lwip-internal cyclic timers.
+ */
 #define LWIP_TIMERS 1
+
+/**
+ * Enable Netconn API (require to use api_lib.c).
+ */
 #define LWIP_NETCONN 0
+
+/**
+ * Enable Socket API (require to use sockets.c).
+ */
 #define LWIP_SOCKET 0
+
+/**
+ * Enable IPv4.
+ */
 #define LWIP_IPV4 1
+
+/**
+ * Enable ICMP module inside the IP stack.
+ */
 #define LWIP_ICMP 1
 #define LWIP_RAND rand
-#define LWIP_DHCP 1
+
+/**
+ * Enable IGMP module inside the IP stack.
+ */
 #define LWIP_IGMP 1
+
+/**
+ * Turn on DNS module. UDP must be available for DNS transport.
+ */
 #define LWIP_DNS 1
 
+/**
+ * Enable DHCP module.
+ */
+#define LWIP_DHCP 1
+
+/**
+ * Should be set to the alignment of the CPU.
+ */
 #define MEM_ALIGNMENT 4
+
+/**
+ * The size of the heap memory. If the application will send
+ * a lot of data that needs to be copied, this should be set high.
+ */
 #define MEM_SIZE 0x30000
 
+/**
+ * Enable code to support static ARP table entries (using
+ * etharp_add_static_entry/etharp_remove_static_entry).
+ */
 #define ETHARP_SUPPORT_STATIC_ENTRIES 1
+
+/**
+ * Enable inter-task protection (and task-vs-interrupt protection)
+ * for certain critical regions during buffer allocation, deallocation
+ * and memory allocation and deallocation.
+ */
 #define SYS_LIGHTWEIGHT_PROT 0
+
+/**
+ * Support a callback function whenever an interface changes its
+ * up/down status (i.e., due to DHCP IP acquisition).
+ */
 #define LWIP_NETIF_STATUS_CALLBACK 1
 
-/* Leave the checksum checking on RX to hardware */
+/**
+ * Set options to 1 to enable checking of checksums in software for incoming
+ * packets. We leave the checksum checking on RX to hardware.
+ */
 #define CHECKSUM_CHECK_IP 0
 #define CHECKSUM_CHECK_UDP 0
 #define CHECKSUM_CHECK_TCP 0
 #define CHECKSUM_CHECK_ICMP 0
 #define CHECKSUM_CHECK_ICMP6 0
 
-#define TCP_MSS 2000 // maximum segment size, max size of a single packet
-#define TCP_WND 0x800000 // tcp window, max data we can receive at once
-#define TCP_SND_BUF TCP_WND // send buffer space
+/**
+ * Set options to 1 to generate checksums in software for outgoing packets.
+ */
+#ifdef NETWORK_HW_HAS_CHECKSUM
+
+/* Leave the checksum checking on tx to hw */
+#define CHECKSUM_GEN_IP 0
+#define CHECKSUM_GEN_UDP 0
+#define CHECKSUM_GEN_TCP 0
+#define CHECKSUM_GEN_ICMP 0
+#define CHECKSUM_GEN_ICMP6 0
+
+#else
+
+#define CHECKSUM_GEN_IP 1
+#define CHECKSUM_GEN_UDP 1
+#define CHECKSUM_GEN_TCP 1
+#define CHECKSUM_GEN_ICMP 1
+#define CHECKSUM_GEN_ICMP6 1
+
+#endif
+
+/**
+ * TCP Maximum segment size. For the receive side, this MSS is advertised
+ * to the remote side when opening a connection. For the transmit size, this
+ * MSS sets an upper limit on the MSS advertised by the remote host.
+ */
+#define TCP_MSS 1460
+
+/**
+ * The size of a TCP window - Maximum data we can receive at once. This
+ * must be at least (2 * TCP_MSS) for things to work well.
+ */
+#define TCP_WND (1000 * TCP_MSS)
+
+/**
+ * TCP sender buffer space (bytes). To achieve good performance, this
+ * should be at least 2 * TCP_MSS.
+ */
+#define TCP_SND_BUF TCP_WND
+
+/**
+ * TCP writable space (bytes). This must be less than TCP_SND_BUF. It is
+ * the amount of space which must be available in the TCP snd_buf for
+ * select to return writable (combined with TCP_SNDQUEUELOWAT).
+ */
 #define TCP_SNDLOWAT TCP_MSS
 
-#define TCP_QUEUE_OOSEQ 1 // hold out-of-sequence packets instead of immediately dropping them
-#define LWIP_TCP_SACK_OUT 1 // support sending selective acknowledgements
+/**
+ * TCP will queue segments that arrive out of order. Define to 0 if your
+ * device is low on memory.
+ */
+#define TCP_QUEUE_OOSEQ 1
 
-#define LWIP_WND_SCALE 1 // support window sizes > 65536
+/**
+ * TCP will support sending selective acknowledgements (SACKs).
+ */
+#define LWIP_TCP_SACK_OUT 1
+
+/**
+ * Set LWIP_WND_SCALE to 1 to enable window scaling.
+ */
+#define LWIP_WND_SCALE 1
+
+/**
+ * Set TCP_RCV_SCALE to the desired scaling factor (shift count in the
+ * range of [0..14]).
+ * When LWIP_WND_SCALE is enabled but TCP_RCV_SCALE is 0, we can use a large
+ * send window while having a small receive window only.
+ */
 #define TCP_RCV_SCALE 12
 
-#define LWIP_TCP_TIMESTAMPS 1 // support tcp timestamp option
+/**
+ * Support the TCP timestamp option.
+ */
+#define LWIP_TCP_TIMESTAMPS 1
 
-#define PBUF_POOL_SIZE 5000
-#define MEMP_NUM_PBUF TCP_SND_QUEUELEN
-#define MEMP_NUM_TCP_SEG TCP_SND_QUEUELEN
-#define MEMP_NUM_SYS_TIMEOUT 512
+/**
+ * The number of buffers in the pbuf pool.
+ */
+#define PBUF_POOL_SIZE 1000
+
+/**
+ * The number of memp struct pbufs (used for PBUF_ROM and PBUF_REF).
+ * If the application sends a lot of data out of ROM (or other static memory),
+ * this should be set high.
+ */
+#define MEMP_NUM_PBUF (10 * TCP_SND_QUEUELEN)
+
+/**
+ * The number of simultaneously queued TCP segments.
+ */
+#define MEMP_NUM_TCP_SEG (10 * TCP_SND_QUEUELEN)
+
+/*
+ * Streams can hang around in FIN_WAIT state for a
+ * while after closing.  Increase the max number of concurrent streams to allow
+ * for a few of these while the next benchmark runs.
+ */
 #define MEMP_NUM_TCP_PCB 100
+
+
+/**
+ * The number of listening TCP connections.
+ * (requires the LWIP_TCP option)
+ */
 #define MEMP_NUM_TCP_PCB_LISTEN 100
+
+/**
+ * The number of struct netconns.
+ */
 #define MEMP_NUM_NETCONN 100
 
-/* Set this to 0 for performance */
+/**
+ * Enable statistics collection in lwip_stats. Set this to 0 for performance.
+ */
 #define LWIP_STATS 0
 
 /* Debugging options */
