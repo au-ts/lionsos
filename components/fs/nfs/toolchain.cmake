9fe583fcf728b4f6046db40cf46dfc05183f3bdb
dumsum <dumsum@pm.me>
2025-08-29 15:35:34 +1000

    Use unpatched libnfs
    Signed-off-by: dumsum <dumsum@pm.me>

---
+++ components/fs/nfs/toolchain.cmake
@@ -0,0 +1,21 @@
+# Copyright 2025, UNSW
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
+set(MUSL $ENV{MUSL})
+set(CC $ENV{CC})
+set(TGT $ENV{TARGET})
+set(CPU $ENV{CPU})
+
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSROOT ${MUSL})
+set(CMAKE_C_COMPILER ${CC})
+set(CMAKE_C_FLAGS_INIT "-nostdlib -mtune=${CPU}")
+set(CMAKE_C_COMPILER_TARGET ${TGT})
+set(CMAKE_CROSSCOMPILING TRUE)
+
+# Prevent CMake from finding and using libraries or programs from the host system.
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
