afd40f5065bb8984a652c75b1d5bfe41ab265dc9
dumsum <dumsum@pm.me>
2025-09-10 17:30:26 +1000

    Add Hello World wasm example
    Signed-off-by: dumsum <dumsum@pm.me>

---
+++ components/wamr/wamr.c
@@ -0,0 +1,149 @@
+#include <microkit.h>
+
+#include <sddf/serial/queue.h>
+#include <sddf/serial/config.h>
+#include <sddf/i2c/config.h>
+#include <sddf/i2c/queue.h>
+#include <sddf/timer/config.h>
+#include <sddf/network/config.h>
+#include <sddf/network/queue.h>
+#include <sddf/network/lib_sddf_lwip.h>
+#include <lions/fs/config.h>
+#include <lions/fs/protocol.h>
+#include <lions/firewall/arp.h>
+#include <lions/firewall/common.h>
+#include <lions/firewall/config.h>
+#include <lions/firewall/queue.h>
+#include <lions/posix/posix.h>
+#include <lions/util.h>
+
+#include <stdlib.h>
+
+#include "platform_common.h"
+#include "wasm_export.h"
+
+#include "wamr.h"
+
+#include "app_wasm.h"
+
+#define TIMEOUT (1 * NS_IN_MS)
+
+__attribute__((__section__(".serial_client_config"))) serial_client_config_t serial_config;
+__attribute__((__section__(".timer_client_config"))) timer_client_config_t timer_config;
+__attribute__((__section__(".net_client_config"))) net_client_config_t net_config;
+__attribute__((__section__(".lib_sddf_lwip_config"))) lib_sddf_lwip_config_t lib_sddf_lwip_config;
+__attribute__((__section__(".fs_client_config"))) fs_client_config_t fs_config;
+__attribute__((__section__(".i2c_client_config"))) i2c_client_config_t i2c_config;
+__attribute__((__section__(".fw_webserver_config"))) fw_webserver_config_t fw_config;
+
+bool net_enabled;
+bool i2c_enabled;
+bool fs_enabled;
+bool firewall_enabled;
+
+fs_queue_t *fs_command_queue;
+fs_queue_t *fs_completion_queue;
+char *fs_share;
+
+serial_queue_handle_t serial_rx_queue_handle;
+serial_queue_handle_t serial_tx_queue_handle;
+
+net_queue_handle_t net_rx_handle;
+net_queue_handle_t net_tx_handle;
+
+fw_queue_t rx_active;
+fw_queue_t rx_free;
+fw_queue_t arp_req_queue;
+fw_queue_t arp_resp_queue;
+
+i2c_queue_handle_t i2c_queue_handle;
+
+static const void *app_instance_main(wasm_module_inst_t module_inst) {
+    const char *exception;
+
+    wasm_application_execute_main(module_inst, 0, NULL);
+    exception = wasm_runtime_get_exception(module_inst);
+    return exception;
+}
+
+static int wamr_main() {
+    char error_buf[128] = {0};
+
+    printf("WAMR | Initialising runtime...\n");
+    if (!wasm_runtime_init()) {
+        printf("Init runtime environment failed.\n");
+        return -1;
+    }
+
+    wasm_module_t wasm_module = NULL;
+    printf("WAMR | Loading module...\n");
+    if (!(wasm_module = wasm_runtime_load(app_wasm, app_wasm_len, error_buf, sizeof(error_buf)))) {
+        printf("%s\n", error_buf);
+        return -1;
+    }
+
+    struct InstantiationArgs2 *inst_args;
+    printf("WAMR | Creating instantiate args...\n");
+    if (!wasm_runtime_instantiation_args_create(&inst_args)) {
+        printf("failed to create instantiate args\n");
+        return -1;
+    }
+
+    uint32 stack_size = 8192;
+    uint32 heap_size = 4096;
+    printf("WAMR | Setting stack and heap size for instantiate args...\n");
+    wasm_runtime_instantiation_args_set_default_stack_size(inst_args, stack_size);
+    wasm_runtime_instantiation_args_set_host_managed_heap_size(inst_args, heap_size);
+
+    wasm_module_inst_t wasm_module_inst = NULL;
+    printf("WAMR | Instantiating module...\n");
+    if (!(wasm_module_inst = wasm_runtime_instantiate_ex2(wasm_module, inst_args, error_buf, sizeof(error_buf)))) {
+        printf("%s\n", error_buf);
+        return -1;
+    }
+
+    printf("WAMR | Destroying instantiate args...\n");
+    wasm_runtime_instantiation_args_destroy(inst_args);
+
+    const char *exception = NULL;
+    printf("WAMR | Running module...\n");
+    if ((exception = app_instance_main(wasm_module_inst))) {
+        printf("%s\n", exception);
+        return -1;
+    }
+
+    return 0;
+}
+
+void notified(microkit_channel ch) {}
+
+void init(void) {
+    assert(serial_config_check_magic(&serial_config));
+    assert(timer_config_check_magic(&timer_config));
+    net_enabled = net_config_check_magic(&net_config);
+    fs_enabled = fs_config_check_magic(&fs_config);
+
+    if (serial_config.rx.queue.vaddr != NULL) {
+        serial_queue_init(&serial_rx_queue_handle, serial_config.rx.queue.vaddr, serial_config.rx.data.size,
+                          serial_config.rx.data.vaddr);
+    }
+    serial_queue_init(&serial_tx_queue_handle, serial_config.tx.queue.vaddr, serial_config.tx.data.size,
+                      serial_config.tx.data.vaddr);
+
+    firewall_enabled = (fw_config.rx_active.queue.vaddr != NULL);
+
+    if (fs_enabled) {
+        fs_command_queue = fs_config.server.command_queue.vaddr;
+        fs_completion_queue = fs_config.server.completion_queue.vaddr;
+        fs_share = fs_config.server.share.vaddr;
+    }
+
+    i2c_enabled = i2c_config_check_magic(&i2c_config);
+    if (i2c_enabled) {
+        i2c_queue_handle = i2c_queue_init(i2c_config.virt.req_queue.vaddr, i2c_config.virt.resp_queue.vaddr);
+    }
+
+    libc_init();
+    int wamr_ret = wamr_main();
+    printf("WAMR exited with %d\n", wamr_ret);
+}
---
+++ examples/hello-wasm/hello-wasm.mk
@@ -0,0 +1,142 @@
+BOARD_DIR := $(MICROKIT_SDK)/board/$(MICROKIT_BOARD)/$(MICROKIT_CONFIG)
+ARCH := $(shell grep 'CONFIG_SEL4_ARCH  ' $(BOARD_DIR)/include/kernel/gen_config.h | cut -d' ' -f4)
+SDDF := $(LIONSOS)/dep/sddf
+LWIP := $(SDDF)/network/ipstacks/lwip/src
+
+ifeq (${MICROKIT_BOARD},odroidc4)
+	TIMER_DRIVER_DIR := meson
+	ETHERNET_DRIVER_DIR := meson
+	SERIAL_DRIVER_DIR := meson
+	CPU := cortex-a55
+else ifeq (${MICROKIT_BOARD},maaxboard)
+	TIMER_DRIVER_DIR := imx
+	ETHERNET_DRIVER_DIR := imx
+	SERIAL_DRIVER_DIR := imx
+	CPU := cortex-a53
+else ifeq (${MICROKIT_BOARD},qemu_virt_aarch64)
+	TIMER_DRIVER_DIR := arm
+	ETHERNET_DRIVER_DIR := virtio
+	SERIAL_DRIVER_DIR := arm
+	CPU := cortex-a53
+	QEMU := qemu-system-aarch64
+else
+$(error Unsupported MICROKIT_BOARD given)
+endif
+
+TOOLCHAIN := clang
+CC := clang
+LD := ld.lld
+AR := llvm-ar
+RANLIB := llvm-ranlib
+OBJCOPY := llvm-objcopy
+MICROKIT_TOOL ?= $(MICROKIT_SDK)/bin/microkit
+PYTHON ?= python3
+DTC := dtc
+
+ifeq ($(ARCH),aarch64)
+	CFLAGS_ARCH := -mcpu=$(CPU)
+	TARGET := aarch64-none-elf
+else ifeq ($(ARCH),riscv64)
+	CFLAGS_ARCH := -march=rv64imafdc
+	TARGET := riscv64-none-elf
+else
+$(error Unsupported ARCH given)
+endif
+
+ifeq ($(strip $(TOOLCHAIN)), clang)
+	CFLAGS_ARCH += -target $(TARGET)
+endif
+
+WAMR=$(LIONSOS)/components/wamr
+
+METAPROGRAM := $(HELLO_WASM_DIR)/meta.py
+DTS := $(SDDF)/dts/$(MICROKIT_BOARD).dts
+DTB := $(MICROKIT_BOARD).dtb
+
+IMAGES := timer_driver.elf  wamr.elf \
+	  serial_driver.elf serial_virt_tx.elf
+
+SYSTEM_FILE := hello-wasm.system
+
+CFLAGS := \
+	-mtune=$(CPU) \
+	-mstrict-align \
+	-ffreestanding \
+	-O2 \
+	-g3 \
+	-MD \
+	-MP \
+	-Wall \
+	-Wno-unused-function \
+	-Wno-bitwise-op-parentheses \
+	-Wno-shift-op-parentheses \
+	-I$(BOARD_DIR)/include \
+	$(CFLAGS_ARCH) \
+	-DBOARD_$(MICROKIT_BOARD) \
+	-I$(LIONSOS)/include \
+	-I$(SDDF)/include \
+	-I$(SDDF)/include/microkit
+
+include $(LIONSOS)/lib/libc/libc.mk
+
+LDFLAGS := -L$(BOARD_DIR)/lib -L$(LIONS_LIBC)/lib
+LIBS := -lmicrokit -Tmicrokit.ld -lc libsddf_util_debug.a
+
+IMAGE_FILE := hello-wasm.img
+REPORT_FILE := report.txt
+
+all: $(IMAGE_FILE)
+${IMAGES}: $(LIONS_LIBC)/lib/libc.a libsddf_util_debug.a
+
+CHECK_FLAGS_BOARD_MD5:=.board_cflags-$(shell echo -- ${CFLAGS} ${BOARD} ${MICROKIT_CONFIG} | shasum | sed 's/ *-//')
+
+${CHECK_FLAGS_BOARD_MD5}:
+	-rm -f .board_cflags-*
+	touch $@
+
+#wasm app
+include ${HELLO_WASM_DIR}/hello-world/hello-world.mk
+
+include $(WAMR)/wamr.mk
+
+%.o: %.c
+	${CC} ${CFLAGS} -c -o $@ $<
+
+SDDF_LIBC_INCLUDE := $(LIONS_LIBC)/include
+
+SDDF_MAKEFILES := ${SDDF}/util/util.mk \
+		  ${SDDF}/drivers/timer/${TIMER_DRIVER_DIR}/timer_driver.mk \
+		  ${SDDF}/drivers/serial/${SERIAL_DRIVER_DIR}/serial_driver.mk \
+		  ${SDDF}/network/lib_sddf_lwip/lib_sddf_lwip.mk \
+		  ${SDDF}/serial/components/serial_components.mk
+
+include ${SDDF_MAKEFILES}
+
+$(DTB): $(DTS)
+	$(DTC) -q -I dts -O dtb $(DTS) > $(DTB)
+
+$(SYSTEM_FILE): $(METAPROGRAM) $(IMAGES) $(DTB)
+	$(PYTHON) $(METAPROGRAM) --sddf $(SDDF) --board $(MICROKIT_BOARD) --dtb $(DTB) --output . --sdf $(SYSTEM_FILE)
+	$(OBJCOPY) --update-section .device_resources=serial_driver_device_resources.data serial_driver.elf
+	$(OBJCOPY) --update-section .serial_driver_config=serial_driver_config.data serial_driver.elf
+	$(OBJCOPY) --update-section .serial_virt_tx_config=serial_virt_tx.data serial_virt_tx.elf
+	$(OBJCOPY) --update-section .device_resources=timer_driver_device_resources.data timer_driver.elf
+	$(OBJCOPY) --update-section .timer_client_config=timer_client_wamr.data wamr.elf
+	$(OBJCOPY) --update-section .serial_client_config=serial_client_wamr.data wamr.elf
+
+$(IMAGE_FILE) $(REPORT_FILE): $(IMAGES) $(SYSTEM_FILE)
+	$(MICROKIT_TOOL) $(SYSTEM_FILE) --search-path $(BUILD_DIR) --board $(MICROKIT_BOARD) --config $(MICROKIT_CONFIG) -o $(IMAGE_FILE) -r $(REPORT_FILE)
+
+qemu: ${IMAGE_FILE}
+	$(QEMU) -machine virt,virtualization=on \
+		-cpu cortex-a53 \
+		-serial mon:stdio \
+		-device loader,file=$(IMAGE_FILE),addr=0x70000000,cpu-num=0 \
+		-m size=2G \
+		-nographic \
+		-global virtio-mmio.force-legacy=false
+
+FORCE: ;
+
+${SDDF_MAKEFILES} &:
+	cd ${LIONSOS}; git submodule update --init dep/sddf
