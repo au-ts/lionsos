9bff82caf2ad5984b611064ee7dfd6ee332cccdb
Krishnan Winter <krishnan-git@wintermail.me>
2025-10-13 15:25:40 +1100

    examples: fix building of gdb example
    Signed-off-by: Krishnan Winter <krishnan-git@wintermail.me>

---
+++ examples/gdb/net_debugger/debugger.mk
@@ -0,0 +1,13 @@
+debugger.o: $(TOP)/net_debugger/debugger.c libsddf_util.a
+	$(CC) -c $(CFLAGS) $(TOP)/net_debugger/debugger.c -o $@ # There is something weird with $@ on the second build here???
+
+tcp.o: $(TOP)/net_debugger/tcp.c
+	$(CC) -c $(CFLAGS) $(TOP)/net_debugger/tcp.c -o $@
+
+DEBUGGER_OBJS := debugger.o tcp.o
+
+export DEPS := $(DEBUGGER_OBJS:.o=.d)
+
+${DEBUGGER_OBJS}: ${CHECKfFLAGS_BOARD_MD5}
+debugger.elf: $(DEBUGGER_OBJS) libsddf_util.a libvspace.a lib_sddf_lwip.a libgdb.a libco.a libvspace.a
+	$(LD) $(LDFLAGS) $(DEBUGGER_OBJS) libsddf_util.a lib_sddf_lwip.a libgdb.a libvspace.a libco.a $(LIBS) -o $@
