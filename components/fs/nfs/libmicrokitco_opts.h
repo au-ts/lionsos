ff8c38fb13492fd90f4cbb8407e863b11834dbc4
dumsum <dumsum@pm.me>
2025-10-24 14:57:48 +1100

    libc: Update initialisation API
    Clients now need to initialise the library with a specific set of
    socket operations.
    
    Signed-off-by: dumsum <dumsum@pm.me>

---
+++ components/fs/nfs/libmicrokitco_opts.h
@@ -0,0 +1,7 @@
+/*
+ * Copyright 2025, UNSW
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+// not needed; this is only to compile tcp.c which #include <libmicrokitco.h>
+#define LIBMICROKITCO_MAX_COTHREADS 2
--- components/fs/nfs/nfs.c
+++ components/fs/nfs/nfs.c
@@ -26,7 +26,7 @@
 #include <lions/fs/config.h>
 
 #include <lions/util.h>
-#include <lions/posix/tcp.h>
+#include <lions/sock/tcp.h>
 
 #include "nfs.h"
 #include "config.h"
@@ -48,6 +48,20 @@ char *fs_share;
 
 struct nfs_context *nfs;
 
+net_queue_handle_t net_rx_handle;
+net_queue_handle_t net_tx_handle;
+
+extern libc_socket_config_t socket_config;
+
+static bool network_ready = false;
+
+static void netif_status_callback(char *ip_addr) {
+    printf("%s: %s:%d:%s: DHCP request finished, IP address for %s is: %s\r\n", microkit_name, __FILE__, __LINE__,
+           __func__, microkit_name, ip_addr);
+
+    network_ready = true;
+}
+
 void notified(microkit_channel ch) {
     if (ch == timer_config.driver_id) {
         sddf_lwip_process_rx();
@@ -58,16 +72,16 @@ void notified(microkit_channel ch) {
             int socket_index = socket_index_of_fd(nfs_fd);
             int revents = nfs_which_events(nfs);
             int sevents = 0;
-            if (tcp_socket_hup(socket_index)) {
+            if (socket_config.tcp_socket_hup(socket_index)) {
                 sevents |= POLLHUP;
             }
-            if (tcp_socket_err(socket_index)) {
+            if (socket_config.tcp_socket_err(socket_index)) {
                 sevents |= POLLERR;
             }
-            if (revents & POLLOUT && tcp_socket_writable(socket_index)) {
+            if (revents & POLLOUT && socket_config.tcp_socket_writable(socket_index)) {
                 sevents |= POLLOUT;
             }
-            if (revents & POLLIN && tcp_socket_readable(socket_index)) {
+            if (revents & POLLIN && socket_config.tcp_socket_readable(socket_index)) {
                 sevents |= POLLIN;
             }
             if (sevents) {
@@ -89,7 +103,7 @@ void notified(microkit_channel ch) {
     // If we leave any commands in the queue, we can't rely on another client
     // notification to cause us to try to reprocess those commands, hence we
     // try to process commands unconditionally on any notification
-    if (tcp_ready()) {
+    if (network_ready) {
         process_commands();
     }
     sddf_lwip_maybe_notify();
@@ -106,8 +120,19 @@ void init(void)
 
     serial_queue_init(&serial_tx_queue_handle, serial_config.tx.queue.vaddr, serial_config.tx.data.size, serial_config.tx.data.vaddr);
 
-    libc_init();
+    libc_init(&socket_config);
     continuation_pool_init();
-    tcp_init_0();
+
+    net_queue_init(&net_rx_handle, net_config.rx.free_queue.vaddr, net_config.rx.active_queue.vaddr,
+                   net_config.rx.num_buffers);
+    net_queue_init(&net_tx_handle, net_config.tx.free_queue.vaddr, net_config.tx.active_queue.vaddr,
+                   net_config.tx.num_buffers);
+    net_buffers_init(&net_tx_handle, 0);
+
+    sddf_lwip_init(&lib_sddf_lwip_config, &net_config, &timer_config, net_rx_handle, net_tx_handle, NULL, printf,
+                   netif_status_callback, NULL, NULL, NULL);
+
+    sddf_lwip_maybe_notify();
+
     sddf_timer_set_timeout(timer_config.driver_id, TIMEOUT);
 }
