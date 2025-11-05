f7f89113f643acd8873c0818eb7ada5dc57d787f
Ivan Velickovic <i.velickovic@unsw.edu.au>
2025-11-05 13:15:53 +1100

    Revert "micropython: temporarily regress I2C functionality"
    This reverts commit a25d0dafb6de0f0feb150c3d02a483d563642174.
    
    Now that we have sDDF updated to the latest and the appropriate
    I2C changes to get it working, we can revert this regression.
    
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- components/micropython/modmachine.c
+++ components/micropython/modmachine.c
@@ -14,7 +14,7 @@
 
 static const mp_rom_map_elem_t machine_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_machine) },
-    // { MP_ROM_QSTR(MP_QSTR_I2C), MP_ROM_PTR(&machine_i2c_type) },
+    { MP_ROM_QSTR(MP_QSTR_I2C), MP_ROM_PTR(&machine_i2c_type) },
 };
 
 static MP_DEFINE_CONST_DICT(machine_module_globals, machine_module_globals_table);
