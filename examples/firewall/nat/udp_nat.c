aa182f08d8e7b2d6ba260a8ab4cec49e9f3ec67c
debater-coder <hamzah@syedahmed.net>
2026-01-15 14:55:51 +1100

    firewall: map dma region to read packet
    The NAT component now has read/write access to the DMA region where the
    packets are stored.
    
    Signed-off-by: debater-coder <hamzah@syedahmed.net>

--- examples/firewall/nat/udp_nat.c
+++ examples/firewall/nat/udp_nat.c
@@ -14,7 +14,6 @@
 #include <lions/firewall/udp.h>
 #include <lions/firewall/queue.h>
 
-__attribute__((__section__(".net_client_config"))) net_client_config_t net_config;
 __attribute__((__section__(".fw_nat_config"))) fw_nat_config_t nat_config;
 
 /* Incoming packets from filter */
@@ -23,27 +22,49 @@ fw_queue_t filter_queue;
 /* Outgoing packets to router */
 fw_queue_t router_queue;
 
+/* Virtual address of rx buffer data region */
+uintptr_t data_vaddr;
+
 seL4_MessageInfo_t protected(microkit_channel ch, microkit_msginfo msginfo) {}
 
-void notified(microkit_channel ch) {
+static void translate(void) {
     net_buff_desc_t buffer;
 
+    /* Incoming packet from filter */
+    int err = fw_dequeue(&filter_queue, &buffer);
+    assert(!err);
+
+    sddf_printf("[UDP NAT] intercepted packet\n");
+    sddf_printf("[UDP NAT] buffer %p\n", &buffer);
+
+    uintptr_t pkt_vaddr = data_vaddr + buffer.io_or_offset;
+    ipv4_hdr_t *ip_hdr = (ipv4_hdr_t *)(pkt_vaddr + IPV4_HDR_OFFSET);
+    udp_hdr_t *udp_hdr = (udp_hdr_t *)(pkt_vaddr + transport_layer_offset(ip_hdr));
+
+    sddf_printf("[UDP NAT] src: %s:%u\n", ipaddr_to_string(ip_hdr->src_ip, ip_addr_buf0), udp_hdr->src_port);
+    sddf_printf("[UDP NAT] dst: %s:%u\n", ipaddr_to_string(ip_hdr->dst_ip, ip_addr_buf0), udp_hdr->dst_port);
+
+    /* Send packet out to router */
+    fw_enqueue(&router_queue, &buffer);
+    assert(!err);
+
+    microkit_notify(nat_config.router.ch);
+}
+
+void notified(microkit_channel ch) {
     if (ch == nat_config.filter.ch) {
-        /* Incoming packet from filter */
-        int err = fw_dequeue(&filter_queue, &buffer);
-        assert(!err);
-
-        sddf_printf("[UDP NAT] intercepted packet");
-
-        fw_enqueue(&router_queue, &buffer);
-        assert(!err);
-
-        microkit_notify(nat_config.router.ch);
+        translate();
+    } else {
+        sddf_dprintf("[UDP NAT]: Received notification on unknown channel: %d!\n", ch);
     }
 }
 
 void init(void) {
-     sddf_printf("[UDP NAT] Starting...\n");
+     sddf_printf("[UDP NAT] Started.\n");
+
+     data_vaddr = (uintptr_t)nat_config.data.region.vaddr;
+
+     sddf_printf("[UDP NAT] vaddr: %p\n", &data_vaddr);
 
      sddf_printf("router queue vaddr: %p, capacity: %d\n", nat_config.router.queue.vaddr, nat_config.router.capacity);
      fw_queue_init(&router_queue, nat_config.router.queue.vaddr,
