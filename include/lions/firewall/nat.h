400775b634e8f18c1388f430f97a824342071b8b
debater-coder <hamzah@syedahmed.net>
2026-01-16 15:55:30 +1100

    firewall: use shared table to NAT UDP returning traffic
    The port table is indexed by (port - base_port) and stores original
    request information. On outbound requests, the next available entry has
    its src and dst ip and port stored, and returning traffic will have its
    translation reversed to send the traffic to the original sender.
    
    Signed-off-by: debater-coder <hamzah@syedahmed.net>

---
+++ include/lions/firewall/nat.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <os/sddf.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <sddf/util/util.h>
+#include <sddf/network/util.h>
+#include <lions/firewall/common.h>
+#include <lions/firewall/array_functions.h>
+
+/**
+ * Stores original source and destination corresponding to a NAT ephemeral port.
+ */
+typedef struct fw_nat_port_mapping {
+    /* original source ip of traffic */
+    uint32_t src_ip;
+    /* destination ip of traffic */
+    uint32_t dst_ip;
+    /* original source port of traffic */
+    uint16_t src_port;
+    /* destination port of traffic */
+    uint16_t dst_port;
+} fw_nat_port_mapping_t;
+
+typedef struct fw_nat_port_table {
+    uint16_t size;
+    fw_nat_port_mapping_t mappings[];
+} fw_nat_port_table_t;
