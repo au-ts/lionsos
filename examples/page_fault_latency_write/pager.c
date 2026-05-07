e14bf4569539aa466891f5789c9cf8b1fe5e8e70
Joshua Shim <joshua.shim2004@gmail.com>
2026-05-07 20:25:49 +1000

    almost there

--- examples/page_fault_latency_write/pager.c
+++ examples/page_fault_latency_write/pager.c
@@ -5,7 +5,10 @@
 
 #define ROUND_DOWN_TO_4K(x) ((x) & ~(4096 - 1))
 #define BRK_START 0x8000000000 
-
+#define MAX_PDS 64
+#define NUM_PT_ENTRIES 300
+#define MMAP_START 0x9000000000
+typedef unsigned long long Cap;
 typedef struct microkit_data {
     Cap frame_cap;
     uintptr_t pd_idx;
@@ -16,8 +19,8 @@ uint64_t unmapped_frames_addr;
 uint64_t num_frames;
 
 // assumes that there is only one child.
-inline frame_pd_id *get_frame_cap(uintptr_t fault_addr) {
-    return ((frame_pd_id *)(unmapped_frames_addr + ((ROUND_DOWN_TO_4K(fault_addr - BRK_START) / 4096) * sizeof(frame_pd_id)))).cap;
+inline Cap get_frame_cap(uintptr_t fault_addr) {
+    return ((frame_pd_id *)(unmapped_frames_addr + ((ROUND_DOWN_TO_4K(fault_addr - BRK_START) / 4096) * sizeof(frame_pd_id))))->frame_cap;
 }
 
 void init(void)
@@ -35,11 +38,11 @@ seL4_Bool fault(microkit_child child, microkit_msginfo msginfo, microkit_msginfo
     uint32_t pd_idx = child;
 
     if (is_write) {
-        microkit_arm_page_map_rw()
+        microkit_arm_page_map_rw(get_frame_cap(fault_addr), vspaces[pd_idx], ROUND_DOWN_TO_4K(fault_addr));
     } else {
-        microkit_arm_page_map_ro(get_frame_cap(fault_addr), vspaces[pd_idx], ROUND_DOWN_TO_4K(fault_addr)))
+        microkit_arm_page_map_ro(get_frame_cap(fault_addr), vspaces[pd_idx], ROUND_DOWN_TO_4K(fault_addr));
     }
-    return seL4_Talse;
+    return seL4_True;
 }
 
 // NOT USED BELOW:
